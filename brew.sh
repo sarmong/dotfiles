ec40e0bfeaa0ebcd4c5e0e0e331f2d952d272fe1
sarmong <42828375+sarmong@users.noreply.github.com>
2020-04-10 20:23:34 +0300

    add zathura to brew

--- brew.sh
+++ brew.sh
@@ -10,6 +10,12 @@ brew install git
 brew install bash-completion@2
 brew install neofetch
 
+brew tap zegervdv/zathura
+brew install zathura
+brew install zathura-pdf-poppler
+mkdir -p $(brew --prefix zathura)/lib/zathura
+ln -s $(brew --prefix zathura-pdf-poppler)/libpdf-poppler.dylib $(brew --prefix zathura)/lib/zathura/libpdf-poppler.dylib
+
 # cli utils
 brew install tree
 brew install exiftool
