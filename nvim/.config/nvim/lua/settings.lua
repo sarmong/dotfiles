72f26e3affaf6c2cfa568c1c14ece66f54e4fea5
sarmong <42828375+sarmong@users.noreply.github.com>
2021-05-03 20:12:44 +0300

    update

--- nvim/.config/nvim/lua/settings.lua
+++ nvim/.config/nvim/lua/settings.lua
@@ -22,8 +22,10 @@ vim.o.t_Co = "256" -- Support 256 colors
 vim.o.conceallevel = 0 -- So that I can see `` in markdown files
 vim.cmd('set ts=4') -- Insert 2 spaces for a tab
 vim.cmd('set sw=4') -- Change the number of space characters inserted for indentation
-vim.bo.expandtab = true -- Converts tabs to spaces
-vim.bo.smartindent = true -- Makes indenting smart
+vim.cmd('set expandtab') -- Change the number of space characters inserted for indentation
+vim.cmd('set smartindent') -- Change the number of space characters inserted for indentation
+-- vim.bo.expandtab = true -- Converts tabs to spaces
+-- vim.bo.smartindent = true -- Makes indenting smart
 vim.wo.number = true -- set numbered lines
 vim.wo.relativenumber = true -- set relative number
 vim.wo.cursorline = true -- Enable highlighting of the current line
