deff7f90f1eec3b7ea6b7b6ba0722c5668dd7732
sarmong <42828375+sarmong@users.noreply.github.com>
2021-12-08 18:13:23 +0200

    nvim: set up commentary

--- nvim/.config/nvim/lua/nv-comment/init.lua
+++
@@ -1,7 +0,0 @@
-require('nvim_comment').setup()
-vim.api.nvim_set_keymap("n", "<leader>/", ":CommentToggle<CR>", {noremap=true, silent = true})
-vim.api.nvim_set_keymap("v", "<leader>/", ":CommentToggle<CR>", {noremap=true, silent = true})
-
--- for some reason in vim _ and / are swapped
-vim.api.nvim_set_keymap("n", "<C-_>", ":CommentToggle<CR>", {noremap=true, silent = true})
-vim.api.nvim_set_keymap("v", "<C-_>", ":CommentToggle<CR>", {noremap=true, silent = true})
