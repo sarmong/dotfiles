f6061b53ad252905f5c5f5450eefe1e54669a4d9
sarmong <42828375+sarmong@users.noreply.github.com>
2021-12-18 20:16:50 +0200

    nvim: set up indent-blankline

--- nvim/.config/nvim/lua/plugins/indent-blankline.lua
+++ nvim/.config/nvim/lua/plugins/indent-blankline.lua
@@ -1,7 +1,13 @@
+require("indent_blankline").setup {
+    -- for example, context is off by default, use this to turn it on
+    show_current_context = true,
+    show_current_context_start = true,
+}
+
 vim.g.indent_blankline_buftype_exclude = {'terminal'}
-vim.g.indent_blankline_filetype_exclude = {'man', 'help', 'startify', 'dashboard', 'packer', 'neogitstatus'}
+vim.g.indent_blankline_filetype_exclude = {'man', 'help', 'alpha', 'startify', 'dashboard', 'packer', 'neogitstatus'}
 vim.g.indent_blankline_char = '▏'
--- vim.g.indent_blankline_use_treesitter = true
+vim.g.indent_blankline_use_treesitter = true
 vim.g.indent_blankline_show_trailing_blankline_indent = false
 vim.g.indent_blankline_show_current_context = true
 vim.g.indent_blankline_context_patterns = {
