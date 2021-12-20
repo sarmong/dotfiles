bd6b4e9ad5dc6711cfb09ac37425d9088cd2061e
sarmong <42828375+sarmong@users.noreply.github.com>
2021-12-20 08:34:58 +0200

    nvim: set keymaps to lsp fns

--- nvim/.config/nvim/lua/plugins/lsp/init.lua
+++ nvim/.config/nvim/lua/plugins/lsp/init.lua
@@ -6,9 +6,12 @@ require("plugins.lsp.null-ls")
 
 local lsp_installer = require("nvim-lsp-installer")
 local configs = require("plugins.lsp.lspconfig")
+local lsp_fns = require("plugins.lsp.functions")
 
 lsp_installer.on_server_ready(function(server)
   -- Use the server's custom settings, if they exist, otherwise default to the default options
   local server_options = configs.server_opt[server.name] and configs.server_opt[server.name]() or configs.default_opt
   server:setup(server_options)
+
+  lsp_fns.disable_virtual_text()
 end)
