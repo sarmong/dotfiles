e0afc2ac6c485022370faee8192fbcd88606fbbb
sarmong <42828375+sarmong@users.noreply.github.com>
2021-12-23 16:38:36 +0200

    nvim: disable formmatting with tsserver

--- nvim/.config/nvim/lua/plugins/lsp/functions.lua
+++ nvim/.config/nvim/lua/plugins/lsp/functions.lua
@@ -1,23 +1,6 @@
 local fns = {}
 
 fns.format = function()
-  local clients = vim.lsp.buf_get_clients(0)
-  if #clients > 1 then
-    -- check if multiple clients, and if null-ls is setup
-    for _, c1 in pairs(clients) do
-      if c1.name == "null-ls" then
-        -- if null-ls then disable others
-        for _, c2 in pairs(clients) do
-          -- print(c2.name, c2.resolved_capabilities.document_formatting)
-          if c2.name ~= "null-ls" then
-            c2.resolved_capabilities.document_formatting = false
-          end
-        end
-        -- no need to continue first loop
-        break
-      end
-    end
-  end
   vim.api.nvim_command("lua vim.lsp.buf.formatting_sync(nil, 1000)")
   print("Formatted")
 end
