c162da093de719bb6e18e69a466f58e7333d568c
sarmong <42828375+sarmong@users.noreply.github.com>
2021-12-23 20:51:51 +0200

    nvim: use lua fns for git

--- nvim/.config/nvim/lua/plugins/git/functions.lua
+++ nvim/.config/nvim/lua/plugins/git/functions.lua
@@ -1,60 +1,89 @@
 local gitsigns = require("gitsigns")
-local fns = { gitsigns = {} }
 
-fns.gitsigns.next_hunk = function()
-  gitsigns.next_hunk()
-end
+local gitsigns = {
+  next_hunk = function()
+    gitsigns.next_hunk()
+  end,
 
-fns.gitsigns.prev_hunk = function()
-  gitsigns.prev_hunk()
-end
+  prev_hunk = function()
+    gitsigns.prev_hunk()
+  end,
 
-fns.gitsigns.stage_hunk = function()
-  gitsigns.stage_hunk()
-end
+  stage_hunk = function()
+    gitsigns.stage_hunk()
+  end,
 
-fns.gitsigns.undo_stage_hunk = function()
-  gitsigns.undo_stage_hunk()
-end
+  undo_stage_hunk = function()
+    gitsigns.undo_stage_hunk()
+  end,
 
-fns.gitsigns.reset_hunk = function()
-  gitsigns.reset_hunk()
-end
+  reset_hunk = function()
+    gitsigns.reset_hunk()
+  end,
 
-fns.gitsigns.reset_buffer = function()
-  gitsigns.reset_buffer()
-end
+  reset_buffer = function()
+    gitsigns.reset_buffer()
+  end,
 
-fns.gitsigns.preview_hunk = function()
-  gitsigns.preview_hunk()
-end
+  preview_hunk = function()
+    gitsigns.preview_hunk()
+  end,
 
-fns.gitsigns.hover_blame = function()
-  gitsigns.blame_line({ full = true })
-end
+  hover_blame = function()
+    gitsigns.blame_line({ full = true })
+  end,
 
-fns.gitsigns.stage_buffer = function()
-  gitsigns.stage_buffer()
-end
+  stage_buffer = function()
+    gitsigns.stage_buffer()
+  end,
 
-fns.gitsigns.reset_buffer_index = function()
-  gitsigns.reset_buffer_index()
-end
+  reset_buffer_index = function()
+    gitsigns.reset_buffer_index()
+  end,
 
-fns.gitsigns.toggle_signs = function()
-  gitsigns.toggle_signs()
-end
+  toggle_signs = function()
+    gitsigns.toggle_signs()
+  end,
 
-fns.gitsigns.toggle_numhl = function()
-  gitsigns.toggle_numhl()
-end
+  toggle_numhl = function()
+    gitsigns.toggle_numhl()
+  end,
 
-fns.gitsigns.toggle_linehl = function()
-  gitsigns.toggle_linehl()
-end
+  toggle_linehl = function()
+    gitsigns.toggle_linehl()
+  end,
 
-fns.gitsigns.toggle_word_diff = function()
-  gitsigns.toggle_word_diff()
-end
+  toggle_word_diff = function()
+    gitsigns.toggle_word_diff()
+  end,
+}
 
-return fns
+local github = {
+  open_file = function()
+    vim.cmd("GBrowse") -- rhubarb.vim
+  end,
+}
+
+local git = {
+  toggle_blame = function()
+    vim.api.nvim_command("GitBlameToggle") -- gitblame.nvim
+  end,
+
+  open_diff_vsplit = function()
+    vim.api.nvim_command("Gvdiffsplit") -- fugitive
+  end,
+
+  show_log = function()
+    vim.api.nvim_command("Git log") -- fugitive
+  end,
+
+  show_status = function()
+    vim.api.nvim_command("Gstatus") -- fugitive
+  end,
+}
+
+return {
+  gitsigns = gitsigns,
+  github = github,
+  git = git,
+}
