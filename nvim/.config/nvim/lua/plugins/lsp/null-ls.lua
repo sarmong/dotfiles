3a0287946e99df3cb7ad7e89060225f47024d488
sarmong <42828375+sarmong@users.noreply.github.com>
2021-12-23 16:30:24 +0200

    nvim: set up css lsp

--- nvim/.config/nvim/lua/plugins/lsp/null-ls.lua
+++ nvim/.config/nvim/lua/plugins/lsp/null-ls.lua
@@ -5,7 +5,9 @@ null_ls.setup({
   sources = {
     null_ls.builtins.formatting.stylua,
     null_ls.builtins.formatting.prettierd,
+    null_ls.builtins.formatting.stylelint,
     null_ls.builtins.diagnostics.eslint,
+    null_ls.builtins.diagnostics.stylelint,
     null_ls.builtins.completion.spell,
   },
   on_attach = function(client)
