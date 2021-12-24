6ef92e7aa7424c1ee41de6fed4aef01f2ed6ad78
sarmong <42828375+sarmong@users.noreply.github.com>
2021-12-24 11:36:30 +0200

    nvim: set up buffer fns

--- nvim/.config/nvim/lua/plugins/barbar.lua
+++ nvim/.config/nvim/lua/plugins/barbar.lua
@@ -1,5 +1,59 @@
 -- @TODO add some other options to whichKey
 
+local fns = {
+  next = function()
+    vim.cmd("BufferNext")
+  end,
+
+  prev = function()
+    vim.cmd("BufferPrevious")
+  end,
+
+  move_next = function()
+    vim.cmd("BufferMoveNext")
+  end,
+
+  move_prev = function()
+    vim.cmd("BufferMovePrevious")
+  end,
+
+  pin = function()
+    vim.cmd("BufferPin")
+  end,
+
+  close = function()
+    vim.cmd("BufferClose")
+  end,
+
+  go_to = function(n)
+    vim.cmd("BufferGoto " .. n)
+  end,
+
+  pick = function()
+    vim.cmd("BufferPick")
+  end,
+
+  close_all_but_current = function()
+    vim.cmd("BufferCloseAllButCurrent")
+  end,
+
+  close_all_but_pinned = function()
+    vim.cmd("BufferCloseAllButPinned")
+  end,
+
+  close_all_to_the_left = function()
+    vim.cmd("BufferCloseBuffersLeft")
+  end,
+
+  close_all_to_the_right = function()
+    vim.cmd("BufferCloseBuffersRight")
+  end,
+
+  order_by_directory = function()
+    vim.cmd("BufferOrderByDirectory")
+  end,
+}
+
 vim.api.nvim_set_keymap(
   "n",
   "<TAB>",
@@ -121,4 +175,4 @@ tree.close = function()
   require("nvim-tree").close()
 end
 
-return tree
+return fns
