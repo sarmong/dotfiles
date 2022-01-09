6655970365459de70bfbba9546a8b4312f9a0752
michael <michale.ns32@gmail.com>
2022-01-09 17:49:21 +0200

    nvim: set up vim ls

--- nvim/.config/nvim/lsp/servers/init.lua
+++ nvim/.config/nvim/lsp/servers/init.lua
@@ -2,6 +2,7 @@ require("lsp.servers.ts")
 require("lsp.servers.lua")
 require("lsp.servers.css")
 require("lsp.servers.bash")
+require("lsp.servers.viml")
 -- require("lsp.servers.remark")
 
 require("lsp.servers.null-ls")
