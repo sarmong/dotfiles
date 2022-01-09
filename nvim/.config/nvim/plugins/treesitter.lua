133b86cc429cfb740080e4e9275a8d6ccdc1f310
michael <michale.ns32@gmail.com>
2022-01-09 20:19:33 +0200

    nvim: set up markdown plugin

--- nvim/.config/nvim/plugins/treesitter.lua
+++ nvim/.config/nvim/plugins/treesitter.lua
@@ -1,7 +1,7 @@
 local parser_configs = require("nvim-treesitter.parsers").get_parser_configs()
 
 require("nvim-treesitter.configs").setup({
-  ensure_installed = "all", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
+  ensure_installed = "maintained", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
   highlight = {
     enable = true, -- false will disable the whole extension
   },
@@ -9,7 +9,7 @@ require("nvim-treesitter.configs").setup({
   -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
   -- Using this option may slow down your editor, and you may see some duplicate highlights.
   -- Instead of true it can also be a list of languages
-  additional_vim_regex_highlighting = false,
+  additional_vim_regex_highlighting = { "markdown" },
 
   autotag = { enable = true },
   rainbow = {
