9767a7361f5fd4ba9982b2849c438d98c22643cb
michael <michale.ns32@gmail.com>
2022-01-15 22:34:01 +0200

    scripts(keymaps): fixes

--- scripts/bin/setup/keyboard/init.sh
+++ scripts/bin/setup/keyboard/init.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env sh
 
-xkbcomp "$(dirname "$0")"/xkbconf "$DISPLAY"
+setxkbmap "us,ru" -option caps:ctrl_modifier -option altwin:swap_alt_win -option altwin:alt_super_win
+## Set Super to Alt
+xmodmap -e "keycode 135 = Alt_R"
 
-## Set Left Control (beware that we swapped control and caps on the previous line) to be Escape when tapped shortly with 1000ms timeout
+## Set to be Escape when tapped shortly with 1000ms timeout
 killall xcape 2>/dev/null
-xcape -t 1000 -e 'Control_L=Escape'
+xcape -t 1000 -e 'Caps_Lock=Escape'
 
 ## Start cursor movement after 250ms and at 45 lines per second
 xset r rate 250 45
@@ -12,11 +14,8 @@ xset r rate 250 45
 ## ---------------------
 ## Old config
 
-## This is now set in xkbconf
-# killall setxkbmap
-# setxkbmap "us, ru" -option caps:ctrl_modifier -option altwin:swap_alt_win -option altwin:alt_super_win
-# xmodmap is apparently pre-xkb and shouldn't be used
-# xmodmap -e "keycode 135 = Alt_R"
+## This is now set by setxkbmap
+# xkbcomp "$(dirname "$0")"/xkbconf.xkb "$DISPLAY"
 
 ## -------------------------
 ## For some guides see
