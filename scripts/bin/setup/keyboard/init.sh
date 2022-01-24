c6ef4fdd2a80b44126c7d66e68dd821e3fecab32
michael <michale.ns32@gmail.com>
2022-01-24 21:38:09 +0200

    setup: set up keyboard with keyd

--- scripts/bin/setup/keyboard/init.sh
+++ scripts/bin/setup/keyboard/init.sh
@@ -1,15 +1,25 @@
 #!/usr/bin/env sh
 
-setxkbmap "us,ru" -option caps:ctrl_modifier -option altwin:swap_alt_win -option altwin:alt_super_win
-## Set Super to Alt
-xmodmap -e "keycode 135 = Alt_R"
+../../services/keyd/init.sh
 
-## Set to be Escape when tapped shortly with 1000ms timeout
-killall xcape 2>/dev/null
-xcape -t 1000 -e 'Caps_Lock=Escape'
+setxkbmap "us,ru"
 
 ## Start cursor movement after 250ms and at 45 lines per second
 xset r rate 250 45
+
+## ---------------------
+## Old config 2
+
+# setxkbmap "us,ru" -option caps:ctrl_modifier -option altwin:swap_alt_win -option altwin:alt_super_win
+# ## Set Super to Alt
+# xmodmap -e "keycode 135 = Alt_R"
+
+# ## Set to be Escape when tapped shortly with 1000ms timeout
+# killall xcape 2>/dev/null
+# xcape -t 1000 -e 'Caps_Lock=Escape'
+
+# ## Start cursor movement after 250ms and at 45 lines per second
+# xset r rate 250 45
 
 ## ---------------------
 ## Old config
