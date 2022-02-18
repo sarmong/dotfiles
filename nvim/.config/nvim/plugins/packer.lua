0bf85008cb02019dd226b9b915e8a1da5d11db04
sarmong <42828375+sarmong@users.noreply.github.com>
2022-02-18 21:31:14 +0200

    nvim: fix packer bootstrap script

--- nvim/.config/nvim/plugins/packer.lua
+++ nvim/.config/nvim/plugins/packer.lua
@@ -1,13 +1,16 @@
-local execute = vim.api.nvim_command
 local fn = vim.fn
 
 local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
 
 if fn.empty(fn.glob(install_path)) > 0 then
-  execute(
-    "!git clone https://github.com/wbthomason/packer.nvim " .. install_path
-  )
-  execute("packadd packer.nvim")
+  packer_bootstrap = fn.system({
+    "git",
+    "clone",
+    "--depth",
+    "1",
+    "https://github.com/wbthomason/packer.nvim",
+    install_path,
+  })
 end
 
 -- vim.cmd 'autocmd BufWritePost plugins.lua PackerCompile' -- Auto compile when there are changes in plugins.lua
@@ -217,4 +220,7 @@ return require("packer").startup(function(use)
   --         vim.fn['firenvim#install'](1)
   --     end
   -- }
+  if packer_bootstrap then
+    require("packer").sync()
+  end
 end)
