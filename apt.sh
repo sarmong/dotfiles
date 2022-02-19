c18a0b10bb80a225c59a73cddafac4ee5692a05b
sarmong <42828375+sarmong@users.noreply.github.com>
2022-02-19 15:58:24 +0200

    apt: use alias for all commands

--- apt.sh
+++ apt.sh
@@ -49,7 +49,7 @@ aptinst lxappearance
 flatinst com.github.Eloston.UngoogledChromium
 
 ## Brave browser
-sudo apt install apt-transport-https curl
+aptinst apt-transport-https curl
 sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
 echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-release.list
 sudo apt update
