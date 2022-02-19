e49a545dc4139c49b6c379802611f2b460c3f3d5
sarmong <42828375+sarmong@users.noreply.github.com>
2022-02-19 11:11:04 +0200

    apt: fixes

--- apt.sh
+++ apt.sh
@@ -2,6 +2,9 @@
 
 alias aptinst="sudo apt install --yes"
 alias brewinst="brew install"
+alias flatinst="flatpak install flathub -y"
+
+sudo apt update && sudo apt upgrade
 
 ### --- Essentials --- ###
 aptinst curl
@@ -12,11 +15,13 @@ aptinst git
 
 ## Homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv) # put brew into PATH
 # @TODO check if this line needed
-# eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
+# aptinst build-essential
 
 ## flatpak
 aptinst flatpak
+sudo apt install --reinstall ca-certificates # this fixes tsl error with flathub
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
 ### --- Install packages --- ###
@@ -31,8 +36,15 @@ aptinst awesome
 aptinst i3lock-fancy
 aptinst arandr
 aptinst lxappearance
-# flatpak install flathub com.github.Eloston.UngoogledChromium
+# flatinst com.github.Eloston.UngoogledChromium
+
+## Brave browser
+sudo apt install apt-transport-https curl
+sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+sudo apt update
 aptinst brave-browser
+
 aptinst trash-cli
 aptinst stow
 aptinst ranger
@@ -46,7 +58,7 @@ aptinst flameshot
 ## kitty installation
 curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin
 # Create a symbolic link to add kitty to PATH (assuming ~/.local/bin is in your PATH)
-ln -s ~/.local/kitty.app/bin/kitty ~/.local/bin/
+mkdir ~/.local/bin && ln -s ~/.local/kitty.app/bin/kitty ~/.local/bin/
 # Place the kitty.desktop file somewhere it can be found by the OS
 cp ~/.local/kitty.app/share/applications/kitty.desktop ~/.local/share/applications
 # Update the path to the kitty icon in the kitty.desktop file
@@ -59,21 +71,25 @@ aptinst xcape
 aptinst xdotool
 
 ### --- Programming tools --- ###
+## needed for treesitter
+aptinst gcc
+aptinst g++
 brewinst neovim
 sudo add-apt-repository universe
 aptinst fonts-firacode
 
 ## Install system version of node
 brewinst node@16
+PATH="/home/linuxbrew/.linuxbrew/opt/node@16/bin:$PATH"
 ## Install nvm
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
 npm i -g yarn
-aptinst gh
 aptinst httpie
-flatpak install flathub com.getpostman.Postman
+flatinst com.getpostman.Postman
 brewinst jesseduffield/lazynpm/lazynpm
 brewinst tig
 brewinst jesseduffield/lazygit/lazygit
+brewinst gh
 aptinst zeal # Documentation browser
 
 sudo snap install sublime-text --classic
@@ -146,8 +162,8 @@ aptinst thunderbird
 sudo snap install skype
 sudo snap install slack --classic
 sudo snap install zoom-client
-flatpak install flathub org.telegram.desktop
-flatpak install flathub com.nextcloud.desktopclient.nextcloud
+flatinst org.telegram.desktop
+flatinst com.nextcloud.desktopclient.nextcloud
 # aptinst spotify
 
 # aptinst bitwarden
