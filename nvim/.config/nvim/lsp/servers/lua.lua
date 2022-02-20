b407b429872611675ebcad24ff6241da44d9fd41
michael <michale.ns32@gmail.com>
2022-02-20 10:47:00 +0200

    nvim: add lua libraries for better completion

--- nvim/.config/nvim/lsp/servers/lua.lua
+++ nvim/.config/nvim/lsp/servers/lua.lua
@@ -7,6 +7,11 @@ local runtime_path = vim.split(package.path, ";")
 table.insert(runtime_path, "lua/?.lua")
 table.insert(runtime_path, "lua/?/init.lua")
 
+local library = vim.api.nvim_get_runtime_file("", true)
+table.insert(library, "/usr/share/nvim/runtime/lua")
+table.insert(library, "/usr/share/nvim/runtime/lua/lsp")
+table.insert(library, "/usr/share/awesome/lib")
+
 configs.server_opt["sumneko_lua"] = function()
   configs.default_opt.settings = {
     Lua = {
@@ -17,12 +22,13 @@ configs.server_opt["sumneko_lua"] = function()
         path = runtime_path,
       },
       diagnostics = {
+        enable = true,
         -- Get the language server to recognize the `vim` global
-        globals = { "vim" },
+        globals = { "vim", "use", "awesome", "client", "root" },
       },
       workspace = {
         -- Make the server aware of Neovim runtime files
-        library = vim.api.nvim_get_runtime_file("", true),
+        library = library,
       },
       -- Do not send telemetry data containing a randomized but unique identifier
       telemetry = {
