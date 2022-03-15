3bb36f386f1e17d3ba5132246ac9735cb7847b80
sarmong <42828375+sarmong@users.noreply.github.com>
2022-03-15 17:28:14 +0200

    scripts: fix suspend script

--- scripts/bin/services/suspend/init.sh
+++ scripts/bin/services/suspend/init.sh
@@ -1,3 +1,4 @@
-sudo cp ~/bin/services/suspend/suspend.service /etc/systemd/system
+sudo ln -s ~/bin/services/suspend/suspend.service /etc/systemd/system
+sudo ln -s ~/bin/services/suspend/suspend.sh /etc/systemd/system
 sudo systemctl daemon-reload
 sudo systemctl enable suspend
