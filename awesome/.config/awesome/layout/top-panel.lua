4823e0abd95d7747876f97f2b90fd74559e1b2e6
michael <michale.ns32@gmail.com>
2022-03-24 22:31:02 +0200

    awesome: add battery tray icon

--- awesome/.config/awesome/layout/top-panel.lua
+++ awesome/.config/awesome/layout/top-panel.lua
@@ -9,6 +9,7 @@ local mat_icon_button = require("widget.material.icon-button")
 local mat_icon = require("widget.material.icon")
 local dpi = require("beautiful").xresources.apply_dpi
 local icons = require("theme.icons")
+local battery_widget = require("widget.battery")
 
 -- Titus - Horizontal Tray
 local systray = wibox.widget.systray()
@@ -96,6 +97,7 @@ local TopPanel = function(s)
 			layout = wibox.layout.fixed.horizontal,
 			wibox.container.margin(systray, dpi(3), dpi(3), dpi(6), dpi(3)),
 			-- Layout box
+      battery_widget,
 			LayoutBox(s),
 			-- Clock
 			clock_widget,
