49ee9c9cf937835607d8ca88f3bcbf804b03fdc3
michael <michale.ns32@gmail.com>
2022-03-26 19:12:55 +0200

    wm: format all awesome code

--- awesome/.config/awesome/rc.lua
+++ awesome/.config/awesome/rc.lua
@@ -30,16 +30,20 @@ _G.root.keys(require("configuration.keys.global"))
 
 -- Signal function to execute when a new client appears.
 _G.client.connect_signal("manage", function(c)
-	-- Set the windows at the slave,
-	-- i.e. put it at the end of others instead of setting it master.
-	if not _G.awesome.startup then
-		awful.client.setslave(c)
-	end
+  -- Set the windows at the slave,
+  -- i.e. put it at the end of others instead of setting it master.
+  if not _G.awesome.startup then
+    awful.client.setslave(c)
+  end
 
-	if _G.awesome.startup and not c.size_hints.user_position and not c.size_hints.program_position then
-		-- Prevent clients from being unreachable after screen count changes.
-		awful.placement.no_offscreen(c)
-	end
+  if
+    _G.awesome.startup
+    and not c.size_hints.user_position
+    and not c.size_hints.program_position
+  then
+    -- Prevent clients from being unreachable after screen count changes.
+    awful.placement.no_offscreen(c)
+  end
 end)
 
 -- Enable sloppy focus, so that focus follows mouse.
@@ -54,8 +58,8 @@ _G.client.connect_signal(
 
 -- Make the focused window have a glowing border
 _G.client.connect_signal("focus", function(c)
-	c.border_color = beautiful.border_focus
+  c.border_color = beautiful.border_focus
 end)
 _G.client.connect_signal("unfocus", function(c)
-	c.border_color = beautiful.border_normal
+  c.border_color = beautiful.border_normal
 end)
--- awesome/.config/awesome/widget/cpu/cpu-meter.lua
+++ awesome/.config/awesome/widget/cpu/cpu-meter.lua
@@ -10,36 +10,37 @@ local total_prev = 0
 local idle_prev = 0
 
 local slider = wibox.widget({
-	read_only = true,
-	widget = mat_slider,
+  read_only = true,
+  widget = mat_slider,
 })
 
 watch([[bash -c "cat /proc/stat | grep '^cpu '"]], 1, function(_, stdout)
-	local user, nice, system, idle, iowait, irq, softirq, steal, guest, guest_nice = stdout:match(
-		"(%d+)%s(%d+)%s(%d+)%s(%d+)%s(%d+)%s(%d+)%s(%d+)%s(%d+)%s(%d+)%s(%d+)%s"
-	)
+  local user, nice, system, idle, iowait, irq, softirq, steal, guest, guest_nice =
+    stdout:match(
+      "(%d+)%s(%d+)%s(%d+)%s(%d+)%s(%d+)%s(%d+)%s(%d+)%s(%d+)%s(%d+)%s(%d+)%s"
+    )
 
-	local total = user + nice + system + idle + iowait + irq + softirq + steal
+  local total = user + nice + system + idle + iowait + irq + softirq + steal
 
-	local diff_idle = idle - idle_prev
-	local diff_total = total - total_prev
-	local diff_usage = (1000 * (diff_total - diff_idle) / diff_total + 5) / 10
+  local diff_idle = idle - idle_prev
+  local diff_total = total - total_prev
+  local diff_usage = (1000 * (diff_total - diff_idle) / diff_total + 5) / 10
 
-	slider:set_value(diff_usage)
+  slider:set_value(diff_usage)
 
-	total_prev = total
-	idle_prev = idle
-	collectgarbage("collect")
+  total_prev = total
+  idle_prev = idle
+  collectgarbage("collect")
 end)
 
 local cpu_meter = wibox.widget({
-	wibox.widget({
-		icon = icons.chart,
-		size = dpi(24),
-		widget = mat_icon,
-	}),
-	slider,
-	widget = mat_list_item,
+  wibox.widget({
+    icon = icons.chart,
+    size = dpi(24),
+    widget = mat_icon,
+  }),
+  slider,
+  widget = mat_list_item,
 })
 
 return cpu_meter
