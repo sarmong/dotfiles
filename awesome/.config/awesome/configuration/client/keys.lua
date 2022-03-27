0f5376284fb3846937e96e24ae75698b75734155
michael <michale.ns32@gmail.com>
2022-03-27 22:18:50 +0300

    wm: raise clients

--- awesome/.config/awesome/configuration/client/keys.lua
+++ awesome/.config/awesome/configuration/client/keys.lua
@@ -7,9 +7,11 @@ local clientKeys = awful.util.table.join(
   awful.key({ modkey }, "f", function(c)
     c.floating = not c.floating
     awful.placement.centered(c)
+    c:raise()
   end, { description = "toggle fullscreen", group = "client" }),
   awful.key({ modkey, "Shift" }, "f", function(c)
     c.maximized = not c.maximized
+    c:raise()
   end, { description = "toggle fullscreen", group = "client" }),
   awful.key({ modkey }, "q", function(c)
     c:kill()
