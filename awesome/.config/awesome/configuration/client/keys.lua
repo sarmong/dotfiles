49916ba886e3a592900de4040eb6dfe377d34ba3
sarmong <42828375+sarmong@users.noreply.github.com>
2022-03-27 22:26:29 +0300

    wm: swap

--- awesome/.config/awesome/configuration/client/keys.lua
+++ awesome/.config/awesome/configuration/client/keys.lua
@@ -4,12 +4,12 @@ local modkey = require("configuration.keys.mod").modKey
 local altkey = require("configuration.keys.mod").altKey
 
 local clientKeys = awful.util.table.join(
-  awful.key({ modkey }, "f", function(c)
+  awful.key({ modkey, "Shift" }, "f", function(c)
     c.floating = not c.floating
     awful.placement.centered(c)
     c:raise()
   end, { description = "toggle fullscreen", group = "client" }),
-  awful.key({ modkey, "Shift" }, "f", function(c)
+  awful.key({ modkey }, "f", function(c)
     c.maximized = not c.maximized
     c:raise()
   end, { description = "toggle fullscreen", group = "client" }),
