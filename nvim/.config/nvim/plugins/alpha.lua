817dc08e6f62604948f653ddcdef65b6521dadc8
sarmong <42828375+sarmong@users.noreply.github.com>
2022-04-01 12:12:03 +0300

    nvim: put cwd files first on dashboard

--- nvim/.config/nvim/plugins/alpha.lua
+++ nvim/.config/nvim/plugins/alpha.lua
@@ -1,6 +1,30 @@
 local alpha = require("alpha")
+local startify_config = require("alpha.themes.startify")
 
-alpha.setup(require("alpha.themes.startify").opts)
+local config = {
+  layout = {
+    { type = "padding", val = 1 },
+    startify_config.section.header,
+    { type = "padding", val = 2 },
+    startify_config.section.top_buttons,
+    startify_config.section.mru_cwd,
+    startify_config.section.mru,
+    { type = "padding", val = 1 },
+    startify_config.section.bottom_buttons,
+    startify_config.section.footer,
+  },
+  opts = {
+    margin = 3,
+    redraw_on_resize = false,
+    setup = function()
+      vim.cmd([[
+            autocmd alpha_temp DirChanged * lua require('alpha').redraw()
+            ]])
+    end,
+  },
+}
+
+alpha.setup(config)
 
 return {
   open_home_page = function()
