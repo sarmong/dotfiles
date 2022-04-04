40fc4dd82304cb32affb7c7c8863b1bd973cb0b3
michael <michale.ns32@gmail.com>
2022-04-04 20:51:46 +0300

    wm: add fullscrenn shortcut

--- awesome/.config/awesome/configuration/client/keys.lua
+++ awesome/.config/awesome/configuration/client/keys.lua
@@ -4,13 +4,18 @@ local modkey = require("configuration.keys.mod").modKey
 local altkey = require("configuration.keys.mod").altKey
 
 local clientKeys = awful.util.table.join(
-  awful.key({ modkey, "Shift" }, "f", function(c)
+  awful.key({ modkey }, "m", function(c)
+    c.maximized = not c.maximized
+    c:raise()
+  end, { description = "toggle maximized", group = "client" }),
+  awful.key({ modkey }, "f", function(c)
     c.floating = not c.floating
+    -- @TODO make window smaller
     awful.placement.centered(c)
     c:raise()
   end, { description = "toggle float", group = "client" }),
-  awful.key({ modkey }, "f", function(c)
-    c.maximized = not c.maximized
+  awful.key({ modkey, "Shift" }, "f", function(c)
+    c.fullscreen = not c.fullscreen
     c:raise()
   end, { description = "toggle fullscreen", group = "client" }),
   awful.key({ modkey, "Shift" }, "s", function(c)
