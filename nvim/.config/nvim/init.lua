e38e0733762819cba436992dc035de013812cc8b
sarmong <42828375+sarmong@users.noreply.github.com>
2022-04-06 13:52:59 +0300

    move packer bootstrap

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -5,6 +5,21 @@ package.path = config_dir
   .. "/?/init.lua;"
   .. package.path
 
+
+local fn = vim.fn
+local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
+if fn.empty(fn.glob(install_path)) > 0 then
+  packer_bootstrap = fn.system({
+    "git",
+    "clone",
+    "--depth",
+    "1",
+    "https://github.com/wbthomason/packer.nvim",
+    install_path,
+  })
+end
+
+
 require("plugins")
 require("settings")
 require("lsp")
--- nvim/.config/nvim/plugins/packer.lua
+++ nvim/.config/nvim/plugins/packer.lua
@@ -1,18 +1,3 @@
-local fn = vim.fn
-
-local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
-
-if fn.empty(fn.glob(install_path)) > 0 then
-  packer_bootstrap = fn.system({
-    "git",
-    "clone",
-    "--depth",
-    "1",
-    "https://github.com/wbthomason/packer.nvim",
-    install_path,
-  })
-end
-
 -- vim.cmd 'autocmd BufWritePost plugins.lua PackerCompile' -- Auto compile when there are changes in plugins.lua
 
 -- require('packer').init({display = {non_interactive = true}})
