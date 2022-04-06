1c1f07532920c46daa70bfddd7aa2e950e8c6fe8
sarmong <42828375+sarmong@users.noreply.github.com>
2022-04-06 13:02:43 +0300

    arch: some fixes

--- scripts/bin/services/keyd/init.sh
+++ scripts/bin/services/keyd/init.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env sh
 
-sudo rm /etc/keyd/default.conf
+[ -f /etc/keyd/default.conf ] && sudo rm /etc/keyd/default.conf
 
 sudo ln -s ~/bin/services/keyd/default.conf /etc/keyd/
 
-sudo service keyd restart
+sudo systemctl enable keyd
+~/bin/services/keyd/restart-keyd
