f0a9da6f136d9e5eeb22ed1ba16c754c734b5634
sarmong <42828375+sarmong@users.noreply.github.com>
2022-04-08 18:28:12 +0300

    wm: updates

--- awesome/.config/awesome/configuration/client/buttons.lua
+++ awesome/.config/awesome/configuration/client/buttons.lua
@@ -8,7 +8,7 @@ return awful.util.table.join(
     c:raise()
   end),
   awful.button({ modkey }, 1, awful.mouse.client.move),
-  awful.button({ modkey }, 3, awful.mouse.client.resize),
+  awful.button({ modkey, "Shift" }, 1, awful.mouse.client.resize),
   awful.button({ modkey }, 4, function()
     awful.layout.inc(1)
   end),
