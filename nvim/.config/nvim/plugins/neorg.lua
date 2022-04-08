c4312811faaa71fd9cc0a6b678f0c6001e38ff83
michael <michale.ns32@gmail.com>
2022-04-08 14:41:52 +0300

    use var for nc dir

--- nvim/.config/nvim/plugins/neorg.lua
+++ nvim/.config/nvim/plugins/neorg.lua
@@ -6,7 +6,7 @@ require("neorg").setup({
     ["core.norg.dirman"] = { -- Manage your directories with Neorg
       config = {
         workspaces = {
-          my_workspace = "~/Nextcloud/Vault",
+          my_workspace = "$XDG_NC_DIR/Vault",
         },
       },
     },
