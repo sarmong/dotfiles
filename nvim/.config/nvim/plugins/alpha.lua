1799f0e91f8dee62f9410fb59f62ff8425a2e773
michael <michale.ns32@gmail.com>
2022-04-17 22:06:28 +0300

    nvim: add restore session button to dashboard

--- nvim/.config/nvim/plugins/alpha.lua
+++ nvim/.config/nvim/plugins/alpha.lua
@@ -1,12 +1,24 @@
 local alpha = require("alpha")
 local startify_config = require("alpha.themes.startify")
 
+local top_buttons = {
+  type = "group",
+  val = {
+    startify_config.button(
+      "r",
+      "Restore session",
+      "<cmd>source ./Session.vim<CR>"
+    ),
+    startify_config.button("e", "New file", "<cmd>ene <CR>"),
+  },
+}
+
 local config = {
   layout = {
     { type = "padding", val = 1 },
     startify_config.section.header,
     { type = "padding", val = 2 },
-    startify_config.section.top_buttons,
+    top_buttons,
     startify_config.section.mru_cwd,
     startify_config.section.mru,
     { type = "padding", val = 1 },
