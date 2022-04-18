a5376f4aa0334efda0e5e2a91044d8c63ed6892e
sarmong <42828375+sarmong@users.noreply.github.com>
2022-04-18 21:38:58 +0300

    nvim: move packer-bootstrap

--- nvim/.config/nvim/plugins/packer.lua
+++ nvim/.config/nvim/plugins/packer.lua
@@ -1,17 +1,3 @@
--- Bootstrap packer
-local fn = vim.fn
-local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
-if fn.empty(fn.glob(install_path)) > 0 then
-  packer_bootstrap = fn.system({
-    "git",
-    "clone",
-    "--depth",
-    "1",
-    "https://github.com/wbthomason/packer.nvim",
-    install_path,
-  })
-end
-
 local packer = require("packer")
 packer.init()
 
@@ -191,7 +177,7 @@ packer.startup(function(use)
   use({ "folke/tokyonight.nvim" })
   -- use("christianchiarulli/nvcode-color-schemes.vim")
 
-  if packer_bootstrap then
+  if _G.packer_bootstrap then
     require("packer").sync()
   end
 end)
