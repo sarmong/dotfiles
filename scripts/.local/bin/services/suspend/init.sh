713f7723bb2e91602bbc1af0d3b86ec1560fa543
sarmong <42828375+sarmong@users.noreply.github.com>
2022-04-19 08:57:56 +0300

    scripts: enable suspend.service immediately

--- scripts/.local/bin/services/suspend/init.sh
+++ scripts/.local/bin/services/suspend/init.sh
@@ -9,4 +9,4 @@ script_file="/etc/systemd/system/suspend.sh"
 sudo ln -s "$XDG_BIN_DIR"/services/suspend/suspend.service /etc/systemd/system
 sudo ln -s "$XDG_BIN_DIR"/services/suspend/suspend.sh /etc/systemd/system
 sudo systemctl daemon-reload
-sudo systemctl enable suspend
+sudo systemctl enable --now suspend
