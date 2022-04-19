795bd1d7c42db1f89f97eb73a830d6b4f8c63f3a
sarmong <42828375+sarmong@users.noreply.github.com>
2022-04-19 14:31:54 +0300

    fix touchpad conf

--- scripts/.local/bin/setup/touchpad/init.sh
+++ scripts/.local/bin/setup/touchpad/init.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
-sudo mkdir /etc/X11/xorg.conf.d
+dir="/etc/X11/xorg.conf.d"
+
+[ ! -d "$dir" ] && sudo mkdir "$dir"
 
 sudo cp "$XDG_BIN_DIR"/setup/touchpad/99-overrides.conf /etc/X11/xorg.conf.d/
