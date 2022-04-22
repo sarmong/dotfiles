ee9cf41ea72902a1f831d367d7acdbd2c7a963e5
sarmong <42828375+sarmong@users.noreply.github.com>
2022-04-22 09:33:50 +0300

    wm: add prompt before quitting a client

--- awesome/.config/awesome/configuration/client/keys.lua
+++ awesome/.config/awesome/configuration/client/keys.lua
@@ -23,7 +23,13 @@ local clientKeys = awful.util.table.join(
     c.ontop = not c.ontop
   end, { description = "toggle fullscreen", group = "client" }),
   awful.key({ modkey }, "q", function(c)
-    c:kill()
+    awful.spawn.with_line_callback("bash -c prompt-yn", {
+      stdout = function(stdout)
+        if stdout == "Yes" then
+          c:kill()
+        end
+      end,
+    })
   end, { description = "close", group = "client" })
 )
 
