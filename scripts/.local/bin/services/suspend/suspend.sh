b3b0734637b809b0f06d8869a86db24e81574665
sarmong <42828375+sarmong@users.noreply.github.com>
2022-04-23 09:30:27 +0300

    don't wake laptop on lid open

--- scripts/.local/bin/services/suspend/suspend.sh
+++ scripts/.local/bin/services/suspend/suspend.sh
@@ -1,14 +1,10 @@
 #!/bin/sh -e
 
-# for device in XHC EHC1 EHC2; do
-#     grep $device /proc/acpi/wakeup | grep enabled > /dev/null && {
-#         echo Disabling wakeup on $device 
-#         sudo sh -c 'echo $device | tee /proc/acpi/wakeup'
-#     }
-# done
+devices='XHC EHC1 EHC2 LID LID0'
 
-# exit 0
-
-if grep XHC /proc/acpi/wakeup | grep enable > /dev/null; then
-  sudo sh -c 'echo XHC > /proc/acpi/wakeup'
-fi
+for device in $devices; do
+  if grep "$device" /proc/acpi/wakeup | grep enable >/dev/null; then
+    echo "Disabling wakeup on $device"
+    sudo sh -c "echo $device > /proc/acpi/wakeup"
+  fi
+done
