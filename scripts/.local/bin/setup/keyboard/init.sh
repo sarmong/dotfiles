8ec00e2239c5010b00a886e5cc2c377b1bbf217e
sarmong <42828375+sarmong@users.noreply.github.com>
2022-04-24 22:07:27 +0300

    set layout with xkbcomp

--- scripts/.local/bin/setup/keyboard/init.sh
+++ scripts/.local/bin/setup/keyboard/init.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env sh
 
-setxkbmap "us,ru"
+# setxkbmap "us,ru"
+xkbcomp ~/.config/xkb/config "$DISPLAY"
 
 ## Start cursor movement after 250ms and at 45 lines per second
 xset r rate 250 45
