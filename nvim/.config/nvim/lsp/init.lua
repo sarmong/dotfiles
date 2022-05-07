2941370f45499474dbe1bb5a8d4cb3cde3a722d4
sarmong <42828375+sarmong@users.noreply.github.com>
2022-05-07 08:52:34 +0300

    nvim: refactor server settings handler

--- nvim/.config/nvim/lsp/init.lua
+++ nvim/.config/nvim/lsp/init.lua
@@ -7,10 +7,11 @@ local configs = require("lsp.lspconfig")
 local lsp_fns = require("lsp.functions")
 
 lsp_installer.on_server_ready(function(server)
-  -- Use the server's custom settings, if they exist, otherwise default to the default options
-  local server_options = configs.server_opt[server.name]
-      and configs.server_opt[server.name]()
-    or configs.default_opt
+  local server_options = vim.tbl_extend(
+    "force",
+    configs.default_opt,
+    configs.server_opt[server.name] or {}
+  )
   server:setup(server_options)
 
   lsp_fns.disable_virtual_text()
--- nvim/.config/nvim/lsp/servers/ts.lua
+++ nvim/.config/nvim/lsp/servers/ts.lua
@@ -3,13 +3,14 @@ local lsp_install = require("lsp.lsp-install")
 
 lsp_install("tsserver")
 
-configs.server_opt["tsserver"] = function()
+configs.server_opt["tsserver"] = {
   -- disable formatting with tsserver, so that null-ls will handle it
-  configs.default_opt.on_attach = function(client, bufnr)
-    configs.on_attach(client, bufnr)
+  on_attach = function(client, bufnr)
+    configs.default_opt.on_attach(client, bufnr)
+
     client.resolved_capabilities.document_formatting = false
     client.resolved_capabilities.document_range_formatting = false
-  end
+  end,
 
-  configs.default_opt.single_file_support = true
-end
+  single_file_support = true,
+}
