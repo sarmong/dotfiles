3b5d8a0319059a2ecbcad4e79e39a6a0d85dda04
sarmong <42828375+sarmong@users.noreply.github.com>
2022-05-12 19:44:52 +0300

    nvim: set local statusline

--- nvim/.config/nvim/plugins/lualine.lua
+++ nvim/.config/nvim/plugins/lualine.lua
@@ -27,7 +27,7 @@ require("lualine").setup({
     section_separators = { left = "", right = "" },
     disabled_filetypes = {},
     always_divide_middle = true,
-    globalstatus = true,
+    globalstatus = false,
   },
   sections = {
     lualine_a = {
