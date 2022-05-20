555ed5d6eae875c4b066f6c27354cf6a28b78a07
sarmong <42828375+sarmong@users.noreply.github.com>
2022-05-20 15:30:27 +0300

    wm: dynamic screen layout calculation

---
+++ scripts/.local/bin/setup/screenlayout/init.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+if [ "$(cat /etc/hostname)" = "dell" ]; then
+  "$XDG_BIN_DIR"/setup/screenlayout/monitor-on-the-left.sh
+else
+  "$XDG_BIN_DIR"/setup/screenlayout/monitor-on-the-left.sh
+fi
