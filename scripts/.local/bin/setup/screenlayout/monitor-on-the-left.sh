e4b6cce99c436d6fb5a4073b88d793cfe0aa359f
michael <michale.ns32@gmail.com>
2022-05-27 21:13:39 +0300

    fix: screen layout script

--- scripts/.local/bin/setup/screenlayout/monitor-on-the-left.sh
+++ scripts/.local/bin/setup/screenlayout/monitor-on-the-left.sh
@@ -1,1 +1,3 @@
-xrandr --output eDP-1 --primary --mode 1366x768 --pos 1998x312 --rotate normal --output HDMI-1 --mode 1920x1080 --pos 0x0 --rotate normal --output DP-1 --off --output HDMI-2 --off
+#!/bin/sh
+
+xrandr --output eDP-1 --mode 1920x1080 --pos 1920x470 --rotate normal --output HDMI-1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output DP-1 --off --output HDMI-2 --off
