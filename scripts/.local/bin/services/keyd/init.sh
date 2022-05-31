f40729f06950f63b97f454df59440f6ee2da9419
sarmong <42828375+sarmong@users.noreply.github.com>
2022-05-31 14:14:26 +0300

    add different keyd config for mac

--- scripts/.local/bin/services/keyd/init.sh
+++ scripts/.local/bin/services/keyd/init.sh
@@ -8,7 +8,13 @@ file="$dir/default.conf"
 
 [ -f "$file" ] || [ -L "$file" ] && sudo mv "$file" "$file"-old
 
-sudo ln -s "$XDG_BIN_DIR"/services/keyd/default.conf /etc/keyd/
+if [ "$(cat /etc/hostname)" = 'air' ]; then
+  file="mac.conf"
+else
+  file="default.conf"
+fi
+
+sudo ln -s "$XDG_BIN_DIR"/services/keyd/"$file" /etc/keyd/default.conf
 
 sudo systemctl enable keyd
 "$XDG_BIN_DIR"/services/keyd/restart-keyd
