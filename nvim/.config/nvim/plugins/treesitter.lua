229318b3b194c590a767bca5bf6dcd74fa7bdb46
michael <michale.ns32@gmail.com>
2022-05-31 16:40:31 +0300

    nvim: install orgmode

--- nvim/.config/nvim/plugins/treesitter.lua
+++ nvim/.config/nvim/plugins/treesitter.lua
@@ -21,6 +21,7 @@ require("nvim-treesitter.configs").setup({
     "lua",
     "make",
     "markdown",
+    "org",
     -- "proto",
     "python",
     "query",
@@ -36,11 +37,12 @@ require("nvim-treesitter.configs").setup({
   },
   highlight = {
     enable = true, -- false will disable the whole extension
+    disable = { "org" },
     -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
     -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
     -- Using this option may slow down your editor, and you may see some duplicate highlights.
     -- Instead of true it can also be a list of languages
-    additional_vim_regex_highlighting = { "markdown" },
+    additional_vim_regex_highlighting = { "markdown", "org" },
   },
 
   textobjects = {
