2a59dc1035b3992ab727a4e77ff83e79d1bc7882
sarmong <42828375+sarmong@users.noreply.github.com>
2022-08-13 09:55:17 +0300

    scripts: improve screen layout script

--- scripts/.local/bin/setup/screenlayout/init.sh
+++ scripts/.local/bin/setup/screenlayout/init.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-if [ "$(cat /etc/hostname)" = "dell" ]; then
-  "$XDG_BIN_DIR"/setup/screenlayout/monitor-on-the-left.sh
-else
-  "$XDG_BIN_DIR"/setup/screenlayout/monitor-on-the-right.sh
-fi
+# if [ "$(cat /etc/hostname)" = "dell" ]; then
+#   "$XDG_BIN_DIR"/setup/screenlayout/monitor-on-the-left.sh
+# else
+"$XDG_BIN_DIR"/setup/screenlayout/monitor-on-the-right.sh
+# fi
