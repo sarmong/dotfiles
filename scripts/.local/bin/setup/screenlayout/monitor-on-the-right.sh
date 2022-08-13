ddeced28a73abfc3315a7bbeb63caf43716d5001
sarmong <42828375+sarmong@users.noreply.github.com>
2022-08-13 09:55:17 +0300

    scripts: improve screen layout script

--- scripts/.local/bin/setup/screenlayout/monitor-on-the-right.sh
+++ scripts/.local/bin/setup/screenlayout/monitor-on-the-right.sh
@@ -1,2 +1,5 @@
 #!/bin/sh
-xrandr --output eDP-1 --mode 1920x1080 --pos 0x377 --rotate normal --output HDMI-2 --primary --mode 1920x1080 --pos 1920x0 --rotate normal --output DP-1 --off --output HDMI-2 --off
+connected=$(xrandr | grep " connected " | awk '{ print$1 }')
+second_monitor=$(echo "$connected" | awk 'NR==2')
+
+xrandr --output eDP-1 --mode 1920x1080 --pos 0x377 --rotate normal --output "$second_monitor" --primary --mode 1920x1080 --pos 1920x0 --rotate normal
