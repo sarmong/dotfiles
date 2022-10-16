e8175c3f2b7d641a71c6dc0e439a3ffb3038713d
sarmong <42828375+sarmong@users.noreply.github.com>
2022-10-16 12:20:40 +0300

    scripts: dynamically connect second monitor

--- scripts/.local/bin/setup/screenlayout/monitor-on-the-right.sh
+++ scripts/.local/bin/setup/screenlayout/monitor-on-the-right.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-connected=$(xrandr | grep " connected " | awk '{ print$1 }')
-second_monitor=$(echo "$connected" | awk 'NR==2')
+second_monitor="$1"
 
 xrandr --output eDP-1 --mode 1920x1080 --pos 0x377 --rotate normal --output "$second_monitor" --primary --mode 1920x1080 --pos 1920x0 --rotate normal
