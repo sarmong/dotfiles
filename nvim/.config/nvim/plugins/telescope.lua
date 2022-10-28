098715df6aaf5971c5fb1a83dbb7a5c3e33ce953
sarmong <42828375+sarmong@users.noreply.github.com>
2022-10-28 10:57:01 +0300

    nvim: delete buffer in telescope normal mode

--- nvim/.config/nvim/plugins/telescope.lua
+++ nvim/.config/nvim/plugins/telescope.lua
@@ -74,6 +74,7 @@ telescope.setup({
         ["<C-j>"] = actions.move_selection_next,
         ["<C-k>"] = actions.move_selection_previous,
         ["<C-q>"] = actions.smart_send_to_qflist + actions.open_qflist,
+        ["<C-y>"] = actions.delete_buffer,
         ["<esc><esc>"] = actions.close,
       },
     },
