114547d24775d2501146d574d13e8e324fe3d0fa
sarmong <42828375+sarmong@users.noreply.github.com>
2022-11-03 20:59:01 +0200

    nvim: fix comment for lua

--- nvim/.config/nvim/plugins/packer.lua
+++ nvim/.config/nvim/plugins/packer.lua
@@ -94,7 +94,11 @@ packer.startup(function(use)
   use({
     "numToStr/Comment.nvim",
     config = function()
-      require("Comment").setup()
+      require("Comment").setup({
+        pre_hook = require(
+          "ts_context_commentstring.integrations.comment_nvim"
+        ).create_pre_hook(),
+      })
     end,
   })
   use({
