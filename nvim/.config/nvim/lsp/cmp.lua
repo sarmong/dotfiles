20af30286ae8005ac01e892166401127b2025448
michael <michale.ns32@gmail.com>
2022-11-03 19:26:36 +0200

    nvim: reformat cmp.lua

--- nvim/.config/nvim/lsp/cmp.lua
+++ nvim/.config/nvim/lsp/cmp.lua
@@ -33,7 +33,8 @@ local kind_icons = {
 local has_words_before = function()
   local line, col = unpack(vim.api.nvim_win_get_cursor(0))
   return col ~= 0
-    and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]
+    and vim.api
+        .nvim_buf_get_lines(0, line - 1, line, true)[1]
         :sub(col, col)
         :match("%s")
       == nil
@@ -94,11 +95,8 @@ cmp.setup({
   formatting = {
     format = function(entry, vim_item)
       -- Kind icons
-      vim_item.kind = string.format(
-        "%s %s",
-        kind_icons[vim_item.kind],
-        vim_item.kind
-      ) -- This concatonates the icons with the name of the item kind
+      vim_item.kind =
+        string.format("%s %s", kind_icons[vim_item.kind], vim_item.kind) -- This concatonates the icons with the name of the item kind
 
       -- Source
       vim_item.menu = ({
