bba82ee08f2d9c93f4216fde5d543c6363618032
michael <michale.ns32@gmail.com>
2022-11-03 19:28:20 +0200

    nvim: fix / and ? completion

--- nvim/.config/nvim/lsp/cmp.lua
+++ nvim/.config/nvim/lsp/cmp.lua
@@ -112,7 +112,8 @@ cmp.setup({
 })
 
 -- Use buffer source for `/` (if you enabled `native_menu`, this won't work anymore).
-cmp.setup.cmdline("/", {
+cmp.setup.cmdline({ "/", "?" }, {
+  mapping = cmp.mapping.preset.cmdline(),
   sources = {
     { name = "buffer" },
   },
