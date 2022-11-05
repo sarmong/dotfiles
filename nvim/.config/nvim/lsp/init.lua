81ca994d8b232454b6da04f06885bd91f6b742fb
sarmong <42828375+sarmong@users.noreply.github.com>
2022-11-05 23:57:21 +0200

    nvim: silence messages on start

--- nvim/.config/nvim/lsp/init.lua
+++ nvim/.config/nvim/lsp/init.lua
@@ -1,7 +1,7 @@
 local lsp_fns = req("lsp.functions")
 
-lsp_fns.disable_virtual_text()
-lsp_fns.enable_format_on_save()
+lsp_fns.disable_virtual_text(true)
+lsp_fns.enable_format_on_save(true)
 
 req("lsp.servers")
 req("lsp.cmp")
