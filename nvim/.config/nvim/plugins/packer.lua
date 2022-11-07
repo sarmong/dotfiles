eebe8ee8712a2f6954b5245fedcb719df842a6b3
michael <michale.ns32@gmail.com>
2022-11-07 21:35:36 +0200

    nvim: update plugins

--- nvim/.config/nvim/plugins/packer.lua
+++ nvim/.config/nvim/plugins/packer.lua
@@ -1,6 +1,6 @@
 local packer = req("packer")
 packer.init({
-  snapshot = "11-06",
+  snapshot = "11-07",
   snapshot_path = vim.fn.resolve(
     vim.fn.stdpath("config") .. "/plugins/snapshots"
   ),
