8699dd032d5c4644f1f4977c798b461211c18936
sarmong <42828375+sarmong@users.noreply.github.com>
2022-11-09 11:02:13 +0200

    nvim: log error when loading module

--- nvim/.config/nvim/utils/req.lua
+++ nvim/.config/nvim/utils/req.lua
@@ -1,5 +1,9 @@
+local function err_handler(err)
+  print(err)
+end
+
 _G.req = function(module_name)
-  local ok, module = pcall(require, module_name)
+  local ok, module = xpcall(require, err_handler, module_name)
 
   if not ok then
     print("There was a problem loading " .. module_name .. " module")
