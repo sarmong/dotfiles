f6206493d378f7bea8793fe8af3ebe89493b9c42
michael <michale.ns32@gmail.com>
2022-11-10 20:59:46 +0200

    nvim: fix code order

--- nvim/.config/nvim/lsp/servers/init.lua
+++ nvim/.config/nvim/lsp/servers/init.lua
@@ -1,6 +1,5 @@
 require("mason").setup()
 require("mason-lspconfig").setup({ automatic_installation = true })
-require("mason-null-ls").setup({ automatic_installation = true })
 
 req("lsp.servers.ts")
 req("lsp.servers.lua")
@@ -11,3 +10,6 @@ req("lsp.servers.python")
 -- req("lsp.servers.remark")
 
 req("lsp.servers.null-ls")
+
+-- has to go after null_ls
+require("mason-null-ls").setup({ automatic_installation = true })
