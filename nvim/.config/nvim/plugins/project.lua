37c606cab72b66bf8fa12a6f20184afc0fd21eca
sarmong <42828375+sarmong@users.noreply.github.com>
2022-11-24 23:05:28 +0200

    nvim: update project.nvim

--- nvim/.config/nvim/plugins/project.lua
+++ nvim/.config/nvim/plugins/project.lua
@@ -1,6 +1,9 @@
 require("project_nvim").setup({
   detection_methods = { "pattern", "lsp" },
+  -- for packages inside monorepo
   patterns = {
+    "package.json",
     ".git",
   },
+  show_hidden = true,
 })
