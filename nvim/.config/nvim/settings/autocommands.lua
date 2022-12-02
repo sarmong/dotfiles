9d8303bbb8b3675f9b1ceb8253524b85fac6c497
sarmong <42828375+sarmong@users.noreply.github.com>
2022-12-02 14:46:14 +0200

    nvim: fix autocommands

--- nvim/.config/nvim/settings/autocommands.lua
+++ nvim/.config/nvim/settings/autocommands.lua
@@ -1,26 +1,29 @@
 local create_augroup = function(name, opts)
   opts = opts or {}
-  vim.api.nvim_create_augroup(name, opts)
+  return vim.api.nvim_create_augroup(name, opts)
 end
 
 local create_autocmd = function(name, opts)
+  if type(opts.group) == "string" then
+    opts = vim.tbl_extend("force", opts, { group = create_augroup(opts.group) })
+  end
   vim.api.nvim_create_autocmd(name, opts)
 end
 
 -- Send WINdow CHanged signal to resize nvim properly when runnin alacritty -e nvim
 create_autocmd("VimEnter", {
-  group = create_augroup("WinCh"),
+  group = "Resize nvim on window resize",
   command = 'silent exec "!kill -s SIGWINCH $PPID"',
 })
 
 create_autocmd({ "BufNewFile", "BufRead" }, {
-  group = create_augroup("rasi_ft"),
+  group = "rasi_ft",
   pattern = "*.rasi",
   command = "set syntax=css",
 })
 
 create_autocmd({ "BufNewFile", "BufRead" }, {
-  group = create_augroup("md_ft"),
+  group = "Mardown options",
   pattern = "*.md",
   callback = function()
     vim.opt_local.wrap = false
@@ -29,7 +32,7 @@ create_autocmd({ "BufNewFile", "BufRead" }, {
 })
 
 create_autocmd("BufReadPost", {
-  group = create_augroup("vim_startup"),
+  group = "Jump to the latest edit position",
   pattern = "*",
   callback = function()
     vim.cmd([[
@@ -40,9 +43,8 @@ create_autocmd("BufReadPost", {
   end,
 })
 
--- Highlight on yank
 create_autocmd("TextYankPost", {
-  group = create_augroup("YankHighlight"),
+  group = "Highlight on yank",
   pattern = "*",
   callback = function()
     vim.highlight.on_yank()
