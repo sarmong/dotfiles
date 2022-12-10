c0f776869903036b26fa3101e439ca3c0106bc8a
sarmong <42828375+sarmong@users.noreply.github.com>
2022-12-10 10:27:52 +0200

    bspwm: improve on-monitor-change script

--- bspwm/.config/bspwm/bin/on-monitor-change.sh
+++ bspwm/.config/bspwm/bin/on-monitor-change.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
 bspc subscribe monitor | while read -r line; do
-  # echo "$line"
-
   event=$(echo "$line" | awk '{print $1}')
 
   case $event in
@@ -14,14 +12,14 @@ bspc subscribe monitor | while read -r line; do
       another_mon=$(grep -Fxvf <(echo "$new_mon") <(echo "$all_mons") | sed -n 1p)
 
       if [ -n "$another_mon" ]; then
-        echo "new_mon" "$new_mon"
-        echo "all_mons" "$all_mons"
-        echo "another_monitor" "$another_mon"
-        echo "--------------------"
         bspc monitor "$new_mon" -d 1 2 3 4 7 8 9 0
-        ~/.config/polybar/launch.sh >/dev/null 2>&1 &
         ~/.config/bspwm/bin/move_windows.sh "$another_mon" "$new_mon"
+
+        ~/.config/polybar/launch.sh >/dev/null 2>&1 &
       fi
+
+      feh --bg-fill "$XDG_PICTURES_DIR"/wallpaper.png
+      killall -SIGUSR1 conky &
       ;;
 
     ## Handle monitor disconnect / disabling
@@ -30,20 +28,16 @@ bspc subscribe monitor | while read -r line; do
       bsp_mons=$(bspc query -M --names)
       removed_mon=$(grep -Fxvf <(echo "$active_mons") <(echo "$bsp_mons") | sed -n 1p)
       another_mon=$(echo "$active_mons" | sed -n 1p)
-      echo "active_mons" "$active_mons"
-      echo "bsp_mons" "$bsp_mons"
-      echo "removed_mon" "$removed_mon"
-      echo "--------------------"
 
       if [ -n "$removed_mon" ] && [ -n "$another_mon" ]; then
-        echo "active_mons" "$active_mons"
-        echo "bsp_mons" "$bsp_mons"
-        echo "removed_mon" "$removed_mon"
-        echo "--------------------"
         ~/.config/bspwm/bin/move_windows.sh "$removed_mon" "$another_mon"
         bspc monitor "$removed_mon" --remove
+
         ~/.config/polybar/launch.sh >/dev/null 2>&1 &
       fi
+
+      feh --bg-fill "$XDG_PICTURES_DIR"/wallpaper.png
+      killall -SIGUSR1 conky &
       ;;
 
   esac
