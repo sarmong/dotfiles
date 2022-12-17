7f4fac149c3b0da56df82bd32be27252e9d23b10
sarmong <42828375+sarmong@users.noreply.github.com>
2022-12-17 20:18:12 +0200

    automate building packages from source

---
+++ packages/build/init.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# shellcheck disable=1007
+script_dir=$(CDPATH= cd -- "$(dirname -- "$0")" && pwd)
+
+cd "$script_dir" || exit 1
+
+./tmux.sh
+./newsboat.sh
---
+++ packages/build/newsboat.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+version="r2.29"
+
+# shellcheck disable=1007
+script_dir=$(CDPATH= cd -- "$(dirname -- "$0")" && pwd)
+
+mkdir -p ~/.local/src/newsboat
+cd ~/.local/src/newsboat || exit 1
+
+wget "https://github.com/newsboat/newsboat/archive/refs/tags/$version.tar.gz"
+aunpack "./$version.tar.gz"
+
+cd "./newsboat-$version" || exit 1
+
+# don't move cursor down on toggle read
+git apply "$script_dir"/newsboat.diff
+
+make
+
+sudo make install
