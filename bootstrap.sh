9c46dfc88ffd94cad8ba190a1bd50a397b727ccd
michael <michale.ns32@gmail.com>
2022-12-17 17:27:41 +0200

    update install scripts

---
+++ bootstrap.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# shellcheck disable=1007
+script_dir=$(CDPATH= cd -- "$(dirname -- "$0")" && pwd)
+cd "$script_dir" || exit 1
+
+./packages/install.sh
+
+./setup.sh
+
+source ./dotconfig/bash/bash-exports
+
+./configure.sh
