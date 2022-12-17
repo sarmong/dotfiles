f1c7584b1c47a271b551c57db35cfa8f204bb7bd
sarmong <42828375+sarmong@users.noreply.github.com>
2022-12-17 17:27:41 +0200

    update install scripts

---
+++ configure.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+curl -fLo "$XDG_DATA_HOME"/vim/autoload/plug.vim --create-dirs \
+  https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+ln -sfT /bin/dash /bin/sh
+
+"$XDG_BIN_DIR"/services/ufw/init.sh
+"$XDG_BIN_DIR"/services/lightdm/init.sh
+"$XDG_BIN_DIR"/services/keyd/init.sh
+"$XDG_BIN_DIR"/services/suspend/init.sh
+
+"$XDG_BIN_DIR"/setup/touchpad/init.sh
+
+sudo systemctl enable NetworkManager
+sudo systemctl enable auto-cpufreq
+sudo systemctl enable bluetooth
+sudo systemctl enable cronie
+sudo systemctl enable betterlockscreen@"$USER"
+
+sudo gpasswd -a "$USER" input
+# This command seems to create a new shell and
+# the execution of the following commands stops until you manually exit.
+# It might be not needed. If libinput-gestures still don't work,
+# I'll need to find a different solution. Adding `&& exit` seems to not help either.
+## newgrp input
