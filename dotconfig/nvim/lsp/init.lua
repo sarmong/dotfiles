77f871655d36465aeb3041c7f2a7b89d22f3f3eb
sarmong <42828375+sarmong@users.noreply.github.com>
2022-12-21 11:55:34 +0200

    nvim: improve diagnostics options

--- dotconfig/nvim/lsp/init.lua
+++ dotconfig/nvim/lsp/init.lua
@@ -1,13 +1,22 @@
 local lsp_fns = req("lsp.functions")
 
-vim.diagnostic.config({
-  float = { border = "rounded" },
-  virtual_text = false,
-})
-
-lsp_fns.enable_format_on_save(true)
-
 req("lsp.servers")
 req("lsp.cmp")
 
 req("fidget").setup({})
+
+lsp_fns.enable_format_on_save(true)
+
+vim.diagnostic.config({
+  float = {
+    border = "rounded",
+    source = "if_many",
+    header = "",
+    prefix = "",
+  },
+  severity_sort = true,
+  virtual_text = false,
+})
+
+vim.lsp.handlers["textDocument/hover"] =
+  vim.lsp.with(vim.lsp.handlers.hover, { border = "rounded" })
