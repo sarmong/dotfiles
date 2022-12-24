0c21d2b4afb227ba978c13da3b36bd9dd2eb7d3c
michael <michale.ns32@gmail.com>
2022-12-24 14:26:47 +0200

    nvim: add one table to rule all servers (almost)

--- dotconfig/nvim/lsp/servers/python.lua
+++
@@ -1,6 +0,0 @@
-local lspconfig = req("lspconfig")
-
-local configs = req("lsp.lspconfig")
-
-lspconfig.pylsp.setup(configs.default_opt)
-lspconfig.pyright.setup(configs.default_opt)
--- dotconfig/nvim/lsp/servers/remark.lua
+++
@@ -1,6 +0,0 @@
-local configs = req("lsp.lspconfig")
-local lsp_install = req("lsp.lsp-install")
-
-lsp_install("remark_ls")
-
--- req("lspconfig").remark_ls.setup({})
