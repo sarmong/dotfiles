d836b6b0027a78115a401b809150057430c93682
sarmong <42828375+sarmong@users.noreply.github.com>
2022-12-24 14:26:47 +0200

    nvim: add one table to rule all servers (almost)

--- dotconfig/nvim/lsp/servers/init.lua
+++ dotconfig/nvim/lsp/servers/init.lua
@@ -1,16 +1,27 @@
+local lspconfig = req("lspconfig")
+local configs = req("lsp.lspconfig")
+
 require("mason").setup()
 require("mason-lspconfig").setup({ automatic_installation = true })
 
 -- https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
 req("lsp.servers.ts")
 req("lsp.servers.lua")
-req("lsp.servers.css")
-req("lsp.servers.bash")
-req("lsp.servers.viml")
-req("lsp.servers.python")
--- req("lsp.servers.remark")
-
 req("lsp.servers.null-ls")
+
+local servers = {
+  "bashls",
+  "clangd",
+  "cssls",
+  "cssmodules_ls",
+  "pylsp",
+  "pyright",
+  "vimls",
+}
+
+for _, server in ipairs(servers) do
+  lspconfig[server].setup(configs.default_opt)
+end
 
 -- has to go after null_ls
 require("mason-null-ls").setup({ automatic_installation = true })
