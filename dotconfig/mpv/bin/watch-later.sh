e9592717b6ac94e759b6e31122043a95eeeb8f36
michael <michale.ns32@gmail.com>
2023-01-02 11:19:52 +0200

    mpv: add watch later script

---
+++ dotconfig/mpv/bin/watch-later.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+wl_file="$XDG_NC_DIR/watch-later.txt"
+
+add() {
+  path=$(mpvc --format '%path%')
+  name=$(yt-dlp --print title "$path")
+  date=$(date '+%Y-%m-%d %H:%M:%S')
+
+  echo -e "$name\n$path\n$date\n" >>"$wl_file"
+}
+
+load() {
+  urls=$(grep "https://" "$wl_file")
+
+  for url in $urls; do
+    echo "loadfile $url append-play" | socat - /tmp/mpvsocket
+  done
+}
+
+case "$1" in
+  add) add ;;
+  load) load ;;
+esac
