1915b0f664e4d374dd20e5b285d30495f9767017
sarmong <42828375+sarmong@users.noreply.github.com>
2023-01-15 11:24:50 +0200

    scripts: add mounter

--- dotconfig/bspwm/autostart.sh
+++ dotconfig/bspwm/autostart.sh
@@ -22,7 +22,7 @@ if [ "$1" = 0 ]; then
   nm-applet &           # indicator # network tray
   xfce4-power-manager & # power manager tray
   conky &
-  udiskie --smart-tray & # mounts drives automatically
+  udiskie &
   playerctld daemon
 
   ## Applications
