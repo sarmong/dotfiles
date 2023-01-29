f53ec2238154dd330ea3a61ecbb2995d48442cf7
sarmong <42828375+sarmong@users.noreply.github.com>
2023-01-29 10:59:48 +0200

    nvim: fix jump to the last edited pos autocmd

--- dotconfig/nvim/settings/autocommands.lua
+++ dotconfig/nvim/settings/autocommands.lua
@@ -19,17 +19,16 @@ autocmd({ "BufNewFile", "BufRead" }, {
   end,
 })
 
--- autocmd("BufReadPost", {
---   group = "Jump to the latest edit position",
---   pattern = "*",
---   callback = function()
---     vim.cmd([[
---       if line("'\"") >= 1 && line("'\"") <= line("$") && &ft !~# 'commit'
---       \ |   exe "normal! g`\""
---       \ | endif
---     ]])
---   end,
--- })
+autocmd("BufReadPost", {
+  group = "Jump to the latest edit position",
+  callback = function()
+    local mark = vim.api.nvim_buf_get_mark(0, '"')
+    local lcount = vim.api.nvim_buf_line_count(0)
+    if mark[1] > 0 and mark[1] <= lcount then
+      pcall(vim.api.nvim_win_set_cursor, 0, mark)
+    end
+  end,
+})
 
 autocmd("TextYankPost", {
   group = "Highlight on yank",
