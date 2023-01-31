32525e049edc6df12666758ffa16a318013bda3e
sarmong <42828375+sarmong@users.noreply.github.com>
2023-01-31 22:49:09 +0200

    nvim: handle all servers in one table

--- dotconfig/nvim/lsp/servers/init.lua
+++ dotconfig/nvim/lsp/servers/init.lua
@@ -1,34 +1,37 @@
 local lspconfig = req("lspconfig")
-
 local default_config = req("lsp.servers.default")
 
 require("mason").setup()
 require("mason-lspconfig").setup({ automatic_installation = true })
 
 -- https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
--- @TODO
-req("lsp.servers.tsserver")
-req("lsp.servers.null-ls")
-
 local servers = {
+  "null_ls",
   "bashls",
   "clangd",
   "cssls",
   "cssmodules_ls",
+  "gopls",
   "pylsp",
   "pyright",
   "sumneko_lua",
+  "tsserver",
   "vimls",
-  "gopls",
 }
 
 for _, server in ipairs(servers) do
-  local ok, config = pcall(require, "lsp.servers." .. server)
-  if not ok then
-    config = {}
-  end
+  local ok, server_config = pcall(require, "lsp.servers." .. server)
 
-  lspconfig[server].setup(vim.tbl_deep_extend("force", default_config, config))
+  -- if file loaded correctly and didn't return a table,
+  -- no need to setup with lspconfig, server was already set up in a file
+  if not ok or type(server_config) == "table" then
+    local config =
+      vim.tbl_deep_extend("force", default_config, ok and server_config or {})
+
+    lspconfig[server].setup(
+      vim.tbl_deep_extend("force", default_config, config)
+    )
+  end
 end
 
 -- has to go after null_ls
