742e512cc39a0d58678a7ac3c5cdd583eb85f158
sarmong <42828375+sarmong@users.noreply.github.com>
2023-02-11 14:30:13 +0200

    nvim: update comment.nvim integration

--- dotconfig/nvim/plugins/comment.lua
+++ dotconfig/nvim/plugins/comment.lua
@@ -15,20 +15,5 @@ vim.api.nvim_set_keymap(
 
 -- taken from https://github.com/JoosepAlviste/nvim-ts-context-commentstring#commentnvim
 req("Comment").setup({
-  pre_hook = function(ctx)
-    local U = req("Comment.utils")
-
-    local location = nil
-    if ctx.ctype == U.ctype.block then
-      location = req("ts_context_commentstring.utils").get_cursor_location()
-    elseif ctx.cmotion == U.cmotion.v or ctx.cmotion == U.cmotion.V then
-      location =
-        req("ts_context_commentstring.utils").get_visual_start_location()
-    end
-
-    return req("ts_context_commentstring.internal").calculate_commentstring({
-      key = ctx.ctype == U.ctype.line and "__default" or "__multiline",
-      location = location,
-    })
-  end,
+  pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
 })
