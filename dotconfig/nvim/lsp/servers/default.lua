991119080f9bdb87afeabb4231daae119bed27ab
michael <michale.ns32@gmail.com>
2023-02-21 19:49:31 +0200

    nvim: use local settings from .nvim.lua

--- dotconfig/nvim/lsp/servers/default.lua
+++ dotconfig/nvim/lsp/servers/default.lua
@@ -16,8 +16,34 @@ local on_attach = function(client, bufnr, elses)
   map("n", "]d", vim.diagnostic.goto_next, opts)
 end
 
+local on_init = function(client)
+  local local_config_file
+  local root_dir
+  for dir in vim.fs.parents(vim.api.nvim_buf_get_name(0)) do
+    if vim.fn.filereadable(dir .. "/.nvim.lua") == 1 then
+      root_dir = dir
+      local_config_file = root_dir .. "/.nvim.lua"
+      break
+    end
+  end
+  if local_config_file then
+    local local_config = dofile(local_config_file)
+    local lsp_conf = local_config[client.name]
+    if lsp_conf then
+      for key, value in pairs(lsp_conf) do
+        local maybe_path = root_dir .. "/" .. value
+        if fn.filereadable(maybe_path) > 0 or fn.isdirectory(maybe_path) then
+          value = maybe_path
+        end
+        client.config.settings[key] = value
+      end
+    end
+  end
+end
+
 local default_config = {
   on_attach = on_attach,
+  on_init = on_init,
   flags = {
     debounce_text_changes = 150,
   },
