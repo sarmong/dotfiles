dd67b72c80ba671d982d6052687f222e4ea76dce
sarmong <42828375+sarmong@users.noreply.github.com>
2023-04-12 09:56:49 +0300

    awesome: add log fns

--- dotconfig/awesome/utils/log.lua
+++ dotconfig/awesome/utils/log.lua
@@ -1,8 +1,30 @@
 local naughty = require("naughty")
 
+function _G.dump(o)
+  if type(o) == "table" then
+    local s = "{ "
+    for k, v in pairs(o) do
+      if type(k) ~= "number" then
+        k = '"' .. k .. '"'
+      end
+      s = s .. "[" .. k .. "] = " .. dump(v) .. ","
+    end
+    return s .. "} "
+  else
+    return tostring(o)
+  end
+end
+
 function _G.log(title, text)
-  naughty.notify({
+  naughty.notification({
     title = "log: " .. tostring(title),
-    text = tostring(text),
+    message = tostring(text),
   })
+end
+
+function _G.log_to_file(text)
+  local file = io.open("~/awesome.log", "a")
+  io.output(file)
+  io.write("[" .. os.date() .. "] " .. text .. "\n")
+  io.close(file)
 end
