883ff9a745acd064f313c0a00fc11319ba8dd001
michael <michale.ns32@gmail.com>
2023-04-16 20:52:21 +0300

    awesome: add floating layout

--- dotconfig/awesome/tags.lua
+++ dotconfig/awesome/tags.lua
@@ -63,6 +63,7 @@ awful.layout.layouts = {
   awful.layout.suit.tile,
   awful.layout.suit.max,
   lain.layout.centerwork,
+  awful.layout.suit.floating,
 }
 
 awful.screen.connect_for_each_screen(function(s)
