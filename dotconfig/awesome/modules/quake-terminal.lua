1099dfb7e94cb1a5c9fa08b6d80d29bcb2e64965
michael <michale.ns32@gmail.com>
2023-11-02 13:50:38 +0200

    wm: add scratch quake terminal

--- dotconfig/awesome/modules/quake-terminal.lua
+++ dotconfig/awesome/modules/quake-terminal.lua
@@ -1,7 +1,8 @@
 local lain = require("lain")
 
-local quake = lain.util.quake({
+local dropdown = lain.util.quake({
   app = os.getenv("TERMINAL"),
+  name = "Dropdown",
   argname = "--class %s",
   followtag = true,
   width = 0.6,
@@ -13,4 +14,24 @@ local quake = lain.util.quake({
   end,
 })
 
-return quake
+local scratch = lain.util.quake({
+  app = os.getenv("TERMINAL"),
+  name = "Scratch",
+  argname = "--class %s",
+  extra = string.format(
+    "--working-directory %s -e %s -c 'nvim scratch.txt ; %s'",
+    os.getenv("XDG_NC_DIR"),
+    os.getenv("SHELL"),
+    os.getenv("SHELL")
+  ),
+  followtag = true,
+  width = 0.6,
+  height = 0.4,
+  horiz = "center",
+  overlap = true,
+  settings = function(c)
+    c.sticky = true
+  end,
+})
+
+return { dropdown = dropdown, scratch = scratch }
