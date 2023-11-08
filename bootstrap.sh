f716ca6290d10c04b2569387c7dcacb69b33bf64
sarmong <42828375+sarmong@users.noreply.github.com>
2023-11-08 13:06:21 +0200

    scripts: exit from bootstrap if not installed properly

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# shellcheck disable=1007
+# shellcheck disable=1007,2181
 script_dir=$(CDPATH= cd -- "$(dirname -- "$0")" && pwd)
 cd "$script_dir" || exit 1
 
@@ -8,6 +8,10 @@ log_dir="$script_dir/logs"
 mkdir "$log_dir"
 
 ./packages/install.sh
+
+if [ $? -gt 0 ]; then
+  exit $?
+fi
 
 ./packages/build/init.sh | tee -a "$log_dir"/bootstrap.log
 
