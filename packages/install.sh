ef937e287adaea2de20b001302e4b2aa8170d25c
sarmong <42828375+sarmong@users.noreply.github.com>
2023-11-08 14:10:13 +0200

    script: refactor install.sh

--- packages/install.sh
+++ packages/install.sh
@@ -1,22 +1,5 @@
 #!/usr/bin/env bash
-
-# shellcheck disable=1007
-script_dir=$(CDPATH= cd -- "$(dirname -- "$0")" && pwd)
-cd "$script_dir" || exit 1
-
-log_dir="$script_dir/logs"
-[ ! -d "$log_dir" ] && mkdir "$log_dir"
-
-os=$(grep -oP '^ID=\K\w+' </etc/os-release)
-
-if [ "$os" = 'arch' ]; then
-  packages_file="./arch.txt"
-elif [ "$os" = 'debian' ]; then
-  packages_file="./debian.txt"
-else
-  echo "$red $os is not currently supported"
-  exit 1
-fi
+# shellcheck disable=2181,2129
 
 # https://stackoverflow.com/questions/16843382/colored-shell-script-output-library
 red='\e[0;31m'
@@ -24,20 +7,13 @@ green='\e[0;32m'
 bi_cyan='\e[1;96m'
 nocol='\e[0m' # Text Reset
 
-failed_packages=()
+# shellcheck disable=1007
+script_dir=$(CDPATH= cd -- "$(dirname -- "$0")" && pwd)
 
-install() {
-  IFS=$' '
-  command="$1"
-  package="$2"
-  echo -e "$green Installing $bi_cyan$package$nocol..."
-  if ! $command "$package" >>"$log_dir"/output.log 2>>"$log_dir"/errors.log; then
-    echo -e "$red An error occured"
-    failed_packages+=("$package")
-  else
-    echo -e "$green Successfully installed $bi_cyan$package$nocol"
-  fi
-}
+log_dir="$script_dir/logs"
+[ ! -d "$log_dir" ] && mkdir "$log_dir"
+
+os=$(grep -oP '^ID=\K\w+' </etc/os-release)
 
 pac() {
   install "sudo pacman -S --noconfirm --needed" "$1"
@@ -57,12 +33,6 @@ pstall() {
 
 nix() {
   install "nix-env --install --attr" "nixpkgs.$1"
-}
-
-finalize() {
-  echo -e "\n"
-  echo -e "$red The following packages failed to install: $nocol"
-  printf "%s\n" "${failed_packages[@]}" | tee -a "$log_dir"/errors.log
 }
 
 install_paru() {
@@ -89,6 +59,7 @@ install_pacstall() {
 
   echo -e "$red Pacstall not found, downloading the script:$nocol"
   curl -fSL https://pacstall.dev/q/install -o ./pacstall-install.sh
+  chmod +x ./pacstall-install.sh
   echo -e "$green Now, manually VERIFY the script and run:"
   echo -e "$bi_cyan sudo bash -c ./pacstall-install.sh"
   exit 1
@@ -107,20 +78,43 @@ install_nix() {
   exit 1
 }
 
+install() {
+  IFS=$' '
+  command="$1"
+  package="$2"
+  echo -e "$green Installing $bi_cyan$package$nocol..."
+  if ! $command "$package" >>"$log_dir"/output.log 2>>"$log_dir"/errors.log; then
+    echo -e "$red An error occured"
+    return 1
+  else
+    echo -e "$green Successfully installed $bi_cyan$package$nocol"
+  fi
+}
+
 trap 'finalize && exit 1' SIGTERM SIGINT SIGQUIT
 
-if [ "$os" == 'arch' ]; then
+cd "$script_dir" || exit 1
+
+if [ "$os" = 'arch' ]; then
+  packages_file="./arch.txt"
+
   sudo pacman -Syy
   install_paru
-fi
+elif [ "$os" = 'debian' ]; then
+  packages_file="./debian.txt"
 
-if [ "$os" == 'debian' ]; then
   install_pacstall
   pacstall -A https://raw.githubusercontent.com/sarmong/pacstall-sarmong/master
   install_nix
+else
+  echo "$red $os is not currently supported"
+  exit 1
 fi
 
 to_install=$(sed 's/#.*$//g' "$packages_file" | sed '/^$/d')
+
+failed_packages=()
+installed_packages=()
 
 IFS=$'\n'
 for package in $to_install; do
@@ -142,14 +136,31 @@ for package in $to_install; do
 
     if [ "$flag" = "n" ]; then
       nix "$package_name"
-    fi
-
-    if [ "$flag" = "p" ]; then
+    elif [ "$flag" = "p" ]; then
       pstall "$package_name"
+    else
+      apt "$package_name"
     fi
+  fi
 
-    apt "$package_name"
+  if [ $? -gt 0 ]; then
+    failed_packages+=("$package")
+  else
+    installed_packages+=("$package")
   fi
 done
+
+finalize() {
+  echo -e "\n"
+  echo -e "$red The following packages failed to install: $nocol"
+  printf "FAILED: \n\n" >>"$log_dir"/packages.log
+  printf "%s\n" "${failed_packages[@]}" | tee -a "$log_dir"/packages.log
+
+  printf "\nINSTALLED: \n\n" >>"$log_dir"/packages.log
+  printf "%s\n" "${installed_packages[@]}" >>"$log_dir"/packages.log
+
+  printf "\nNOT INSTALLED: \n\n" >>"$log_dir"/packages.log
+  grep -Fxvf <(printf "%s\n" "${installed_packages[@]}") <(echo "$to_install") >>"$log_dir"/packages.log
+}
 
 finalize
