569de9732ce600c1c148f8f6bf4d451a3a749ff4
michael <michale.ns32@gmail.com>
2023-11-14 22:38:23 +0200

    lightdm: use gtk-greeter

--- dotlocal/bin/services/lightdm/init.sh
+++ dotlocal/bin/services/lightdm/init.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env sh
 
 conf_file="/etc/lightdm/lightdm.conf"
-greeter_conf="/etc/lightdm/slick-greeter.conf"
+greeter_conf="/etc/lightdm/lightdm-gtk-greeter.conf"
 
 [ -f "$conf_file" ] || [ -L "$conf_file" ] && sudo mv "$conf_file" "$conf_file-old"
 [ -f "$greeter_conf" ] || [ -L "$greeter_conf" ] && sudo mv "$greeter_conf" "$greeter_conf-old"
 
 sudo ln -s "$XDG_BIN_DIR"/services/lightdm/lightdm.conf /etc/lightdm/lightdm.conf
-sudo ln -s "$XDG_BIN_DIR"/services/lightdm/slick-greeter.conf /etc/lightdm/slick-greeter.conf
-sudo ln -s "$XDG_BIN_DIR"/services/lightdm/wp.jpg /etc/lightdm/wp.jpg
+sudo ln -s "$XDG_BIN_DIR"/services/lightdm/lightdm-gtk-greeter.conf /etc/lightdm/lightdm-gtk-greeter.conf
+sudo cp "$XDG_BIN_DIR"/services/lightdm/wp.jpg /etc/lightdm/wp.jpg
 sudo ln -s "$XDG_BIN_DIR"/services/lightdm/Xsession /etc/lightdm/Xsession
 
 sudo systemctl enable lightdm
