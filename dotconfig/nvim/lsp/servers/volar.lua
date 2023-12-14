8f4a67bc480cedcea3463e85ad2f6bcd9e30b907
michael <michale.ns32@gmail.com>
2023-12-14 15:12:47 +0200

    nvim(lsp): disable formatting with volar

--- dotconfig/nvim/lsp/servers/volar.lua
+++ dotconfig/nvim/lsp/servers/volar.lua
@@ -1,3 +1,5 @@
+local default_conf = req("lsp.servers.default")
+
 return {
   filetypes = {
     "typescript",
@@ -7,4 +9,12 @@ return {
     "vue",
     "json",
   },
+
+  on_attach = function(client, bufnr)
+    -- disable formatting, so that null-ls will handle it
+    client.server_capabilities.documentFormattingProvider = false
+    client.server_capabilities.documentRangeFormattingProvider = false
+
+    default_conf.on_attach(client, bufnr)
+  end,
 }
