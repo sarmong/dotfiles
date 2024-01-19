b15067b052b0f5dcfa66a1a8891fd734cdcdf370
sarmong <42828375+sarmong@users.noreply.github.com>
2024-01-19 22:21:25 +0200

    nvim: md highlight queries wip

---
+++ dotconfig/nvim/after/queries/markdown/highlights.scm
@@ -0,0 +1,103 @@
+;; extends
+
+; NOTE: It would be nice to prevent conceals in table cells (because they can
+; mess up pipe alignment) but this is currently impossible since base conceals
+; are handled internally, and there is no way to detect if other inline conceals
+; are within table cells because detection is split among the parsers.
+
+; Bullet points
+; ([(list_marker_minus) (list_marker_plus) (list_marker_star)]
+;  @punctuation.special
+;  (#offset-first-n! @punctuation.special 1)
+;  (#set! conceal "•"))
+; (list
+;   (list_item
+;     (list
+;       (list_item
+;         ([(list_marker_minus) (list_marker_plus) (list_marker_star)]
+;          @punctuation.special
+;          (#offset-first-n! @punctuation.special 1)
+;          (#set! conceal "⭘"))))))
+; (list
+;   (list_item
+;     (list
+;       (list_item
+;         (list
+;           (list_item
+;             ([(list_marker_minus) (list_marker_plus) (list_marker_star)]
+;              @punctuation.special
+;              (#offset-first-n! @punctuation.special 1)
+;              (#set! conceal "◼"))))))))
+; (list
+;   (list_item
+;     (list
+;       (list_item
+;         (list
+;           (list_item
+;             (list
+;               (list_item
+;                 ([(list_marker_minus) (list_marker_plus) (list_marker_star)]
+;                  @punctuation.special
+;                  (#offset-first-n! @punctuation.special 1)
+;                  (#set! conceal "◻"))))))))))
+; (list
+;   (list_item
+;     (list
+;       (list_item
+;         (list
+;           (list_item
+;             (list
+;               (list_item
+;                 (list
+;                   (list_item
+;                     ([(list_marker_minus) (list_marker_plus) (list_marker_star)]
+;                      @punctuation.special
+;                      (#offset-first-n! @punctuation.special 1)
+;                      (#set! conceal "→"))))))))))))
+
+; Checkbox list items
+((task_list_marker_unchecked)
+ @text.todo.unchecked
+ (#offset! @text.todo.unchecked 0 -2 0 0)
+ (#set! conceal "✘"))
+((task_list_marker_checked)
+ @text.todo.checked
+ (#offset! @text.todo.checked 0 -2 0 0)
+ (#set! conceal "✔"))
+(list_item (task_list_marker_checked) (_) @comment)
+
+; Tables
+(pipe_table_header ("|") @punctuation.special (#set! conceal "┃"))
+(pipe_table_delimiter_row ("|") @punctuation.special (#set! conceal "┃"))
+(pipe_table_delimiter_cell ("-") @punctuation.special (#set! conceal "━"))
+((pipe_table_align_left) @punctuation.special (#set! conceal "┣"))
+((pipe_table_align_right) @punctuation.special (#set! conceal "┫"))
+(pipe_table_row ("|") @punctuation.special (#set! conceal "┃"))
+
+; Block quotes
+((block_quote_marker) @punctuation.special
+                      (#offset! @punctuation.special 0 0 0 -1)
+                      (#set! conceal "▐"))
+((block_continuation) @punctuation.special
+                      (#eq? @punctuation.special ">")
+                      (#set! conceal "▐"))
+((block_continuation) @punctuation.special
+                      (#eq? @punctuation.special "> ")
+                      (#offset! @punctuation.special 0 0 0 -1)
+                      (#set! conceal "▐"))
+((block_continuation) @punctuation.special
+                      ; for indented code blocks
+                      (#eq? @punctuation.special ">     ")
+                      (#offset! @punctuation.special 0 0 0 -5)
+                      (#set! conceal "▐"))
+
+; Thematic breaks
+((thematic_break) @punctuation.special
+                  (#offset! @punctuation.special 0 2 0 0)
+                  (#set! conceal "━"))
+((thematic_break) @punctuation.special
+                  (#offset! @punctuation.special 0 1 0 0)
+                  (#set! conceal "━"))
+((thematic_break) @punctuation.special
+                  (#set! conceal "━"))
+
