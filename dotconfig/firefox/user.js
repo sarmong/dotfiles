06af38a59bbd630c9137b71f38ac4b64c9ec06d8
sarmong <42828375+sarmong@users.noreply.github.com>
2024-02-06 19:47:42 +0200

    ff: semicolons are important

--- dotconfig/firefox/user.js
+++ dotconfig/firefox/user.js
@@ -1,2 +1,2 @@
 user_pref("extensions.pocket.enabled", false);
-user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true)
+user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
