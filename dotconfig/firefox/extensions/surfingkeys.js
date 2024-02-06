80970d08c30981c55eb6e67adc1597bc1daf6d91
sarmong <42828375+sarmong@users.noreply.github.com>
2024-02-06 20:15:18 +0200

    ff: add settings

---
+++ dotconfig/firefox/extensions/surfingkeys.js
@@ -0,0 +1,23 @@
+api.addSearchAlias(
+  "i",
+  "wikipedia",
+  "https://en.wikipedia.org/wiki/",
+  "s",
+  "https://en.wikipedia.org/w/api.php?action=opensearch&format=json&formatversion=2&namespace=0&limit=40&search=",
+  function (response) {
+    return JSON.parse(response.text)[1];
+  },
+);
+
+api.mapkey("Y", "Md link", function () {
+  const removables = [" %E2%80%A2 Instagram photos and videos", " - YouTube"];
+  const url = location.href;
+  let title = document.title;
+  removables.forEach((rem) => {
+    if (title.endsWith(rem)) {
+      title = title.slice(0, title.indexOf(rem));
+    }
+  });
+  const link = `[${title}](${url})`;
+  navigator.clipboard.writeText(link);
+});
