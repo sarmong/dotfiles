2782e00ea7f5a2aaaa76c50d8ef8169a1f02b9d3
sarmong <42828375+sarmong@users.noreply.github.com>
2024-02-12 14:33:39 +0200

    scripts: rewrite screenlayout

--- dotlocal/bin/setup/screenlayout/monitor-on-the-right.sh
+++ dotlocal/bin/setup/screenlayout/monitor-on-the-right.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
-second_monitor="$1"
 
-xrandr --output eDP-1 --mode 1920x1080 --pos 0x377 --rotate normal --output "$second_monitor" --primary --mode 1920x1080 --pos 1920x0 --rotate normal
+second_monitor="$1"
