2f7c58dad25ff27658d08928a660291d77ff14ed
sarmong <42828375+sarmong@users.noreply.github.com>
2024-03-29 19:47:53 +0200

    scripts: update bootstrap-server.sh

---
+++ bootstrap-server.sh
@@ -0,0 +1,61 @@
+#!/usr/bin/env bash
+
+user="michael"
+
+if id -u "$user"; then
+  adduser "$user"
+  adduser "$user" sudo
+
+  read -r -p "Do you want to add www user? [y/N]" yn
+  if [ "$yn" = "Y" ] || [ "$yn" = "y" ]; then
+    adduser www
+    chown www:www /var/www
+    echo "Added www user."
+  fi
+
+  echo "Created user.
+  1. exit
+  2. ssh-copy-id -i ~/.ssh/key $user@[ip]
+  3. ssh-copy-id -i ~/.ssh/wwwkey www@[ip]
+  3. ssh $user@[ip]
+  4. Run this file again
+  "
+
+  exit 0
+fi
+
+echo "Updating sudo to use vim by default"
+
+echo "Defaults      editor=/usr/bin/vim, !env_editor" | (sudo su -c 'EDITOR="tee" visudo -f /etc/sudoers.d/editor')
+
+echo "Updating sshd_config with the following values:"
+
+echo "
+PermitRootLogin no
+PasswordAuthentication no
+X11Forwarding no" | sudo tee -a /etc/ssh/sshd_config
+
+PermitRootLogin no
+PasswordAuthentication no
+X11Forwarding no
+
+echo "Installing packages: "
+
+sed 's/#.*$//g' ./server/debian.txt | sed '/^$/d' | xargs sudo apt install -y
+
+## Firewall
+
+echo "Enabling firewall..."
+
+sudo ufw allow 'OpenSSH'
+sudo ufw allow 'Nginx Full'
+sudo ufw allow 80,443/tcp
+sudo ufw enable
+
+echo "Linking files..."
+
+./link.sh
+
+echo "Changin shell to zsh..."
+
+sudo chsh -s /usr/bin/zsh
--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
 mkdir ~/docs
 cd ~/docs
@@ -7,4 +7,10 @@ git clone https://github.com/sarmong/dotfiles.git
 cd dotfiles
 git submodule update --init --recursive
 
-./bootstrap.sh
+read -p "Press 1 or Enter to bootstrap server or 2 to bootstrap desktop" bootstrap
+
+if [ "$bootstrap" = "2" ]; then
+  ./bootstrap.sh
+else
+  ./bootstrap-server.sh
+fi
