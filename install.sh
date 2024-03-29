aabdaac73105e561a747d2b5737140c4d946b3c7
sarmong <42828375+sarmong@users.noreply.github.com>
2024-03-29 20:00:18 +0200

    fix

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/sh
 
 mkdir ~/docs
 cd ~/docs
@@ -7,10 +7,4 @@ git clone https://github.com/sarmong/dotfiles.git
 cd dotfiles
 git submodule update --init --recursive
 
-read -p "Press 1 or Enter to bootstrap server or 2 to bootstrap desktop" bootstrap
-
-if [ "$bootstrap" = "2" ]; then
-  ./bootstrap.sh
-else
-  ./bootstrap-server.sh
-fi
+./bootstrap.sh
