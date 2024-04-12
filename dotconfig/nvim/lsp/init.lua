1dbebe0590ef88a3069913f8c8a68c7d12b75026
sarmong <42828375+sarmong@users.noreply.github.com>
2024-04-12 19:16:32 +0300

    nvim: fix mason tools install

--- dotconfig/nvim/lsp/init.lua
+++ dotconfig/nvim/lsp/init.lua
@@ -8,17 +8,16 @@ req("fidget").setup({})
 local function extractNames(tableList)
   local names = {}
   for _, tbl in ipairs(tableList) do
-    table.insert(names, tbl.name)
+    for _, source in ipairs(tbl) do
+      table.insert(names, source.name)
+    end
   end
   return names
 end
 
 req("mason-tool-installer").setup({
-  ensure_installed = extractNames(
-    vim.tbl_extend(
-      "force",
-      req("conform").list_all_formatters(),
-      req("null-ls").get_sources()
-    )
-  ),
+  ensure_installed = extractNames({
+    req("conform").list_all_formatters(),
+    req("null-ls").get_sources(),
+  }),
 })
