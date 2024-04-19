8cdcab1d6cedfa8e286930384453f6d45c380a86
sarmong <42828375+sarmong@users.noreply.github.com>
2024-04-19 13:44:47 +0300

    nvim: move to scroll

---
+++ dotconfig/nvim/lua/plugins-new/scroll.lua
@@ -0,0 +1,36 @@
+return {
+  { "sarmong/vim-smoothie" },
+  {
+    "karb94/neoscroll.nvim",
+    enabled = false,
+    config = function()
+      req("neoscroll").setup({
+        -- easing_function = "quintic",
+        mappings = {},
+        hide_cursor = false,
+      })
+
+      local t = {}
+      -- Syntax: t[keys] = {function, {function arguments}}
+      -- Use the "sine" easing function
+      t["<C-u>"] = { "scroll", { "-vim.wo.scroll", "true", "200", "circular" } }
+      t["<C-d>"] = { "scroll", { "vim.wo.scroll", "true", "200", "circular" } }
+      -- Use the "circular" easing function
+      t["<C-b>"] = {
+        "scroll",
+        { "-vim.api.nvim_win_get_height(0)", "true", "500", [['circular']] },
+      }
+      t["<C-f>"] = {
+        "scroll",
+        { "vim.api.nvim_win_get_height(0)", "true", "500", [['circular']] },
+      }
+      -- Pass "nil" to disable the easing animation (constant scrolling speed)
+      -- When no easing function is provided the default easing function (in this case "quadratic") will be used
+      t["zt"] = { "zt", { "100" } }
+      t["zz"] = { "zz", { "100" } }
+      t["zb"] = { "zb", { "100" } }
+
+      require("neoscroll.config").set_mappings(t)
+    end,
+  },
+}
