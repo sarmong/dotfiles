6a38f0f4074c983fe883d0480e718cbb0de495f9
sarmong <42828375+sarmong@users.noreply.github.com>
2024-04-20 09:35:53 +0300

    nvim: move remaining plugins

--- dotconfig/nvim/plugins/true_zen.lua
+++
@@ -1,9 +0,0 @@
-return {
-  toggle_ataraxis = function()
-    vim.cmd("TZAtaraxis")
-  end,
-
-  toggle_focus = function()
-    vim.cmd("TZFocus")
-  end,
-}
--- dotconfig/nvim/plugins/visual-multi.lua
+++
@@ -1,3 +0,0 @@
--- disable all mappings expect for C-n
--- This way, it will not interfere with C-up and C-down for window resizing
-vim.g.VM_default_mappings = 0
