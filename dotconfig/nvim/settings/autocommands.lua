3f6deab0c8ecb9da66200d09d18eeefd55589a6c
sarmong <42828375+sarmong@users.noreply.github.com>
2024-04-25 23:18:09 +0300

    nvim: better o for json

--- dotconfig/nvim/settings/autocommands.lua
+++ dotconfig/nvim/settings/autocommands.lua
@@ -76,6 +76,23 @@ autocmd({ "BufNewFile", "BufRead" }, {
   command = "set filetype=jsonc",
 })
 
+autocmd({ "BufNewFile", "BufRead" }, {
+  pattern = { "*.json" },
+  group = "json",
+  callback = function()
+    vim.keymap.set("n", "o", function()
+      local line = vim.api.nvim_get_current_line()
+
+      local should_add_comma = string.find(line, "[^,{[]$")
+      if should_add_comma then
+        return "A,<cr>"
+      else
+        return "o"
+      end
+    end, { buffer = true, expr = true })
+  end,
+})
+
 autocmd("FileType", {
   pattern = "scratch",
   group = "scratch",
