5e98d880fd0e0f2ed1d62975d14e86ce0fca2d55
sarmong <42828375+sarmong@users.noreply.github.com>
2024-04-27 17:03:18 +0300

    nvim: wip disable markdown shortcut links conceal
    for mkdnflow in progress todo

---
+++ dotconfig/nvim/after/queries/markdown_inline/highlights.scm
@@ -0,0 +1,21 @@
+;; extends
+
+; (shortcut_link
+;   [
+;     "["
+;     "-"
+;     "]"
+;   ] @todo_in_progress
+;  (#offset! @todo_in_progress 0 -2 0 0)
+;   (#set! conceal "⧖"))
+
+(shortcut_link
+  [
+    "["
+  ] @markup.link
+  (#set! conceal ""))
+(shortcut_link
+  [
+    "]"
+  ] @markup.link
+  (#set! conceal ""))
