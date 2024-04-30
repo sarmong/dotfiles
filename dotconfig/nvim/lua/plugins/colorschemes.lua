8374f23f712d8eba1a6e42af59378c1e3c66b2de
sarmong <42828375+sarmong@users.noreply.github.com>
2024-04-30 23:28:53 +0300

    scripts: create toggle-theme

--- dotconfig/nvim/lua/plugins/colorschemes.lua
+++ dotconfig/nvim/lua/plugins/colorschemes.lua
@@ -1,4 +1,29 @@
 return {
+  { "sainnhe/everforest" },
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    config = function()
+      -- vim.cmd.colorscheme("tokyonight-day")
+      -- require("catppuccin").setup({
+      --   integrations = {
+      --     cmp = true,
+      --     aerial = true,
+      --     alpha = true,
+      --     barbar = true,
+      --     fidget = true,
+      --     harpoon = true,
+      --     mason = true,
+      --     window_picker = true,
+      --     gitsigns = true,
+      --     nvimtree = true,
+      --     treesitter = true,
+      --     which_key = true,
+      --   },
+      -- })
+    end,
+  },
   { "ellisonleao/gruvbox.nvim" },
   { "sainnhe/gruvbox-material" },
   { "AlphaTechnolog/onedarker.nvim" },
