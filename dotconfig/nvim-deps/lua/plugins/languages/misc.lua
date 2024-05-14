4a6f123125f74394feb2ad87923f27f9d7b78d31
sarmong <42828375+sarmong@users.noreply.github.com>
2024-05-14 20:14:42 +0300

    nvim: install qmk.nvim

--- dotconfig/nvim-deps/lua/plugins/languages/misc.lua
+++ dotconfig/nvim-deps/lua/plugins/languages/misc.lua
@@ -5,4 +5,25 @@ Plugin("kovetskiy/sxhkd-vim")
 Plugin("sarmong/conky-syntax.vim")
 Plugin("elkowar/yuck.vim")
 Plugin("fladson/vim-kitty")
+Plugin("codethread/qmk.nvim")
+req("qmk").setup({
+  name = "meh",
+  variant = "zmk",
+  auto_format_pattern = "*.keymap",
+  layout = {
+    "x x x x x x _ _ _ x x x x x x",
+    "x x x x x x _ _ _ x x x x x x",
+    "x x x x x x _ _ _ x x x x x x",
+    "x x x x x x x _ x x x x x x x",
+    "_ _ _ x x x x _ x x x x _ _ _",
+  },
+  comment_preview = {
+    keymap_overrides = {
+      ["K_VOL_UP"] = "vol+",
+      ["K_VOL_DN"] = "vol-",
+      ["KP_MULTIPLY"] = "*",
+      ["KP_PLUS"] = "+",
+    },
+  },
+})
 -- { "xuhdev/vim-latex-live-preview" },
