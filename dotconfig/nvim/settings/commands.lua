232297839162202220b659a0aeda3f4f4e7a7766
sarmong <42828375+sarmong@users.noreply.github.com>
2024-05-22 00:42:57 +0300

    nvim: set nvim-tree root

--- dotconfig/nvim/settings/commands.lua
+++ dotconfig/nvim/settings/commands.lua
@@ -35,5 +35,7 @@ end, { nargs = "?" })
 
 command("Cd", function()
   local rd = req("modules.root-dir")
-  rd.set_root(rd.get_project_root())
+  local root = rd.get_project_root()
+  rd.set_root(root)
+  req("nvim-tree.api").tree.change_root(root)
 end)
