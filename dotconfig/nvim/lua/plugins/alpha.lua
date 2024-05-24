b95c4e2d53aa8c487417e53ecce07fba6449c929
sarmong <42828375+sarmong@users.noreply.github.com>
2024-05-25 00:03:40 +0300

    nvim: fix alpha

--- dotconfig/nvim/lua/plugins/alpha.lua
+++ dotconfig/nvim/lua/plugins/alpha.lua
@@ -5,54 +5,68 @@ Plugin({
 
 local startify_config = req("alpha.themes.startify")
 
-local session_files = req("modules.sessions").list_session_files()
+local M = {}
 
-local btns = {}
+M.get_config = function()
+  local session_files = req("modules.sessions").list_session_files()
 
-for key, value in ipairs(session_files) do
-  if key > 3 then
-    break
+  local btns = {}
+
+  for key, value in ipairs(session_files) do
+    if key > 3 then
+      break
+    end
+    btns[key] = startify_config.button(
+      "r" .. key,
+      "Restore session " .. value,
+      "<cmd>lua req('modules.sessions').load_session('" .. value .. "')<CR>"
+    )
   end
-  btns[key] = startify_config.button(
-    "r" .. key,
-    "Restore session " .. value,
-    "<cmd>lua req('modules.sessions').load_session('" .. value .. "')<CR>"
-  )
+
+  btns[#btns + 1] = startify_config.button("e", "New file", "<cmd>ene <CR>")
+
+  local top_buttons = {
+    type = "group",
+    val = btns,
+  }
+
+  local config = {
+    layout = {
+      { type = "padding", val = 1 },
+      startify_config.section.header,
+      { type = "padding", val = 2 },
+      top_buttons,
+      startify_config.section.mru_cwd,
+      startify_config.section.mru,
+      { type = "padding", val = 1 },
+      {
+        type = "group",
+        val = { startify_config.button("Q", "Quit", "<cmd>q <CR>") },
+      },
+      startify_config.section.footer,
+    },
+    opts = {
+      margin = 3,
+      redraw_on_resize = false,
+      setup = function()
+        autocmd("DirChanged", {
+          group = "alpha_temp",
+          callback = function()
+            req("alpha").redraw()
+          end,
+        })
+      end,
+      keymap = {
+        press = "<CR>",
+        queue_press = "<M-CR>",
+      },
+    },
+  }
+  return config
 end
 
-btns[#btns + 1] = startify_config.button("e", "New file", "<cmd>ene <CR>")
+req("alpha").setup(M.get_config())
 
-local top_buttons = {
-  type = "group",
-  val = btns,
-}
-
-local config = {
-  layout = {
-    { type = "padding", val = 1 },
-    startify_config.section.header,
-    { type = "padding", val = 2 },
-    top_buttons,
-    startify_config.section.mru_cwd,
-    startify_config.section.mru,
-    { type = "padding", val = 1 },
-    {
-      type = "group",
-      val = { startify_config.button("Q", "Quit", "<cmd>q <CR>") },
-    },
-    startify_config.section.footer,
-  },
-  opts = {
-    margin = 3,
-    redraw_on_resize = false,
-    setup = function()
-      vim.cmd([[
-            autocmd alpha_temp DirChanged * lua req('alpha').redraw()
-            ]])
-    end,
-  },
-}
-
-req("alpha").setup(config)
-
-map("n", "<leader>;", req("alpha").start, { desc = "home screen" })
+map("n", "<leader>;", function()
+  req("alpha").start(false, M.get_config())
+end, { desc = "home screen" })
