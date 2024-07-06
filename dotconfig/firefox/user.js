a16e11ce088023916057a9fd7c8c96e374110165
sarmong <42828375+sarmong@users.noreply.github.com>
2024-07-06 14:40:03 +0300

    firefox: disable autoupdate

--- dotconfig/firefox/user.js
+++ dotconfig/firefox/user.js
@@ -1,3 +1,4 @@
 user_pref("extensions.pocket.enabled", false);
 user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
 user_pref("full-screen-api.ignore-widgets", true);
+user_pref("app.update.auto", false);
