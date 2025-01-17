74829595c1363ef84e91fed06cfbfebfe707e088
sarmong <42828375+sarmong@users.noreply.github.com>
2025-01-17 22:50:41 +0200

    firefox: add startup page

--- ansible/tasks/configure/files/firefox/user.js
+++ ansible/tasks/configure/files/firefox/user.js
@@ -2,3 +2,4 @@ user_pref("extensions.pocket.enabled", false);
 user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
 user_pref("full-screen-api.ignore-widgets", true);
 user_pref("app.update.auto", false);
+user_pref("browser.startup.homepage", "/var/www/cabin/index.html");
