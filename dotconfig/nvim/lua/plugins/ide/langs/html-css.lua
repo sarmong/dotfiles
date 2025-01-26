b05d7d9665b5bea82585cb6bce3e54f7c21cb40e
sarmong <42828375+sarmong@users.noreply.github.com>
2025-01-26 23:20:57 +0200

    nvim: add tailwindcss

--- dotconfig/nvim/lua/plugins/ide/langs/html-css.lua
+++ dotconfig/nvim/lua/plugins/ide/langs/html-css.lua
@@ -4,7 +4,7 @@ contrib.mason({ "cssls", "cssmodules_ls", "prettierd", "prettier" })
 
 contrib.formatters({ "css", "scss", "less" }, { "prettierd", "prettier" })
 contrib.ts_parsers({ "css", "scss" })
-contrib.lsp({ "cssls", "cssmodules_ls" })
+contrib.lsp({ "cssls", "cssmodules_ls", "tailwindcss" })
 
 contrib.ts_parsers("html")
 contrib.formatters("html", { "prettierd", "prettier" })
