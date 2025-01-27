dcd63532041b5f161669a3c5eec50d4207c877d3
sarmong <42828375+sarmong@users.noreply.github.com>
2025-01-27 20:31:16 +0200

    nvim: use only prettierd

--- dotconfig/nvim/lua/plugins/ide/langs/typescript.lua
+++ dotconfig/nvim/lua/plugins/ide/langs/typescript.lua
@@ -16,7 +16,7 @@ contrib.formatters({
   "typescriptreact",
   "vue",
   "astro",
-}, { "prettier", "prettierd" })
+}, { "prettierd" })
 contrib.ts_parsers({ "javascript", "typescript", "tsx", "jsdoc", "astro" })
 
 local get_lsp_root = function(fname)
