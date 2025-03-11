a91e5f3945d7a25b7c54aaac152dbae795a5bb96
sarmong <42828375+sarmong@users.noreply.github.com>
2025-03-11 10:36:51 +0200

    ansible: match media files

--- ansible/tasks/media/files/bt-connect.sh
+++ ansible/tasks/media/files/bt-connect.sh
@@ -2,10 +2,14 @@
 
 DEVICE_NAME="iLoud Micro"
 
+bluetoothctl devices >/var/lib/kodi/bt-connect.log
+
 DEVICE_MAC=$(bluetoothctl devices | grep "$DEVICE_NAME" | awk '{print $2}')
 
 if [ -n "$DEVICE_MAC" ]; then
+  echo "has mac" >>/var/lib/kodi/bt-connect.log
   bluetoothctl connect "$DEVICE_MAC"
+  echo "connected" >>/var/lib/kodi/bt-connect.log
 else
-  echo "Device $DEVICE_NAME not found."
+  echo "Device $DEVICE_NAME not found." >>/var/lib/kodi/bt-connect.log
 fi
