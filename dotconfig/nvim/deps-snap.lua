fbe39b8ca3086d8740d0c0a758513a4f9aa0a377
sarmong <42828375+sarmong@users.noreply.github.com>
2025-03-29 00:46:31 +0200

    nvim: update conform.nvim

--- dotconfig/nvim/deps-snap.lua
+++ dotconfig/nvim/deps-snap.lua
@@ -17,7 +17,7 @@ return {
   ["cmp-treesitter"] = "958fcfa0d8ce46d215e19cc3992c542f576c4123",
   cmp_luasnip = "98d9cb5c2c38532bd9bdb481067b20fea8f32e90",
   ["codi.vim"] = "83b9859aaf8066d95892e01eb9c01571a4b325dd",
-  ["conform.nvim"] = "a6f5bdb78caa305496357d17e962bbc4c0b392e2",
+  ["conform.nvim"] = "f9ef25a7ef00267b7d13bfc00b0dea22d78702d5",
   ["conky-syntax.vim"] = "c008f3a9208b78f18c55b002eed1d45d0444cff0",
   ["diffview.nvim"] = "4516612fe98ff56ae0415a259ff6361a89419b0a",
   ["dressing.nvim"] = "2d7c2db2507fa3c4956142ee607431ddb2828639",
