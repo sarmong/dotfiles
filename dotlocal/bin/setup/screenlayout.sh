f071b95a39c69a8e65237cc2460292f0c38025ff
sarmong <42828375+sarmong@users.noreply.github.com>
2025-04-02 22:10:46 +0300

    scripts: improve screenlayout.sh

--- dotlocal/bin/setup/screenlayout.sh
+++ dotlocal/bin/setup/screenlayout.sh
@@ -5,11 +5,17 @@ second_monitor=$(echo "$connected" | awk 'NR==2')
 laptop_resolution=$([ "$(hostname)" = "sich" ] && echo "1920x1200" || echo "1920x1080")
 
 single_screen() {
-  xrandr --output eDP-1 --primary --mode "$laptop_resolution" --pos 0x0 --rotate normal --output DP-1 --off --output HDMI-1 --off --output DP-2 --off --output HDMI-2 --off --output DP-2-1 --off --output DP-2-2 --off --output DP-2-3 --off
+  xrandr \
+    --output eDP-1 --primary --mode "$laptop_resolution" --pos 0x0 --rotate normal \
+    --output HDMI-1 --off --output HDMI-2 --off \
+    --output DP-1 --off --output DP-2 --off \
+    --output DP-1-1 --off --output DP-1-2 --off --output DP-1-3 --off
 }
 
 second_screen() {
-  xrandr --output "$second_monitor" --primary --mode 1920x1080 --right-of eDP-1 --pos 1920x0 --rotate normal
+  xrandr \
+    --output "$second_monitor" --primary --mode 1920x1080 --pos 1920x0 --rotate normal \
+    --output eDP-1 --mode "$laptop_resolution" --pos 0x224 --rotate normal
 }
 
 if [ "$1" = "-s" ]; then
