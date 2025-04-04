ecd7f6110dc9a5cb0176b493747c1f6780ee1dac
sarmong <42828375+sarmong@users.noreply.github.com>
2025-04-04 23:00:21 +0300

    scripts: disable all possible outpus in screenlayout

--- dotlocal/bin/setup/screenlayout.sh
+++ dotlocal/bin/setup/screenlayout.sh
@@ -9,7 +9,9 @@ single_screen() {
     --output eDP-1 --primary --mode "$laptop_resolution" --pos 0x0 --rotate normal \
     --output HDMI-1 --off --output HDMI-2 --off \
     --output DP-1 --off --output DP-2 --off \
-    --output DP-1-1 --off --output DP-1-2 --off --output DP-1-3 --off
+    --output DP-1-1 --off --output DP-1-2 --off --output DP-1-3 --off \
+    --output DP-2-1 --off --output DP-2-2 --off --output DP-2-3 --off \
+    --output DP-3-1 --off --output DP-3-2 --off --output DP-3-3 --off
 }
 
 second_screen() {
