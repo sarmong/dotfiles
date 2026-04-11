e563555ad35d2ee4e30f7601de8c0376462161ef
sarmong <42828375+sarmong@users.noreply.github.com>
2026-04-11 21:38:29 +0200

    nvim: add restart mapping

--- dotconfig/nvim/settings/keymappings.lua
+++ dotconfig/nvim/settings/keymappings.lua
@@ -152,6 +152,12 @@ mapl({
 map("n", "<C-i>", "<C-i>") -- needed to distinguish tab and c-i in terminals that support it
 map("n", "<tab>", "<nop>")
 
+map("n", "<leader>R", function()
+  local session = vim.fn.stdpath("state") .. "/restart_session.vim"
+  vim.cmd("mksession! " .. vim.fn.fnameescape(session))
+  vim.cmd("restart source " .. vim.fn.fnameescape(session))
+end, { desc = "Restart Neovim" })
+
 -- When pressing * in visual mode - search for the selected text, and not the word
 cmd([[
     function! s:VSetSearch()
